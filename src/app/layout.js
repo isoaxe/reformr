01e2bc79d1e1cc102385473b2d684a429c06e1e2
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-09-27 10:30:26 +0700

    Add fonts to layout
    Add montserrat as the default and work sans as the alternative.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,18 @@
+import { Montserrat, Work_Sans } from 'next/font/google';
 import './globals.css';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const montserrat = Montserrat({
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const work = Work_Sans({
+  variable: '--font-work-sans',
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'Reformr',
@@ -11,7 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${montserrat.className} ${work.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
