63ab68c1d17ea170ca37355a1052c64e795eec9d
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-01 23:54:10 +0700

    Reposition navbar link text
    Align bottom of nav link text with the bottom of the reformr text logo. Then increase the link text lift on hover so that it goes from bottom of bar to centre.

--- src/components/nav-links.js
+++ src/components/nav-links.js
@@ -10,10 +10,10 @@ export default function NavLinks({ setOpen }) {
   const constant = ' h-full flex items-center px-4';
   const active = 'font-bold bg-primary' + constant;
   const dormant =
-    'font-normal hover:text-white hover:pb-2 transition-all' + constant;
+    'font-normal hover:text-white hover:pb-4 transition-all' + constant;
 
   return (
-    <div className="flex h-40 w-full flex-col justify-between text-xl md:h-full md:flex-row">
+    <div className="flex h-40 w-full flex-col justify-between pt-6 text-xl md:h-full md:flex-row">
       <div className="flex h-80 flex-col md:h-full md:flex-row">
         <h6 className={pathname === '/blog' ? active : dormant}>
           <Link href="/blog" onClick={close}>
