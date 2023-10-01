3c0958fdfc799a5524b5af31a9cc9548496ba370
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-01 23:13:34 +0700

    Add reformr logo that links to homepage
    Will improve positioning and style later.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,8 +2,11 @@
 
 import { useState } from 'react';
 import { Fade as Hamburger } from 'hamburger-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import MobileNavbar from './navbar-mobile';
 import NavLinks from './nav-links';
+import logo from 'public/images/text-logo-plain.png';
 
 export default function Navbar() {
   const [isOpen, setOpen] = useState(false);
@@ -12,6 +15,15 @@ export default function Navbar() {
     <>
       <div className="fixed top-0 z-30 flex h-16 w-full flex-row justify-between bg-violet-600 px-9 md:h-24">
         <div className="max-w-400 m-auto flex h-full w-full flex-row items-center justify-between">
+          <Link href="/">
+            <div className="mr-10 w-32 md:w-44">
+              <Image
+                src={logo}
+                alt="Plain monotone version of Reformr text logo."
+                sizes="(max-width: 767px) 8rem, 11rem"
+              />
+            </div>
+          </Link>
           <div className="hidden h-full w-full md:flex">
             <NavLinks setOpen={setOpen} />
           </div>
