4bf19c572095172a3212d1e34cfdab737360abc1
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-01 23:38:52 +0700

    Change navbar text logo opacity on hover
    This makes the logo on the navbar go slightly purple when hovered to signify interactivity. Important that this is obvious as it's the only navigation to the homepage.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Fade as Hamburger } from 'hamburger-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import MobileNavbar from './navbar-mobile';
 import NavLinks from './nav-links';
 import logo from 'public/images/text-logo-plain.png';
@@ -11,12 +12,18 @@ import logo from 'public/images/text-logo-plain.png';
 export default function Navbar() {
   const [isOpen, setOpen] = useState(false);
 
+  const pathname = usePathname();
+
+  // Logo styles.
+  const active = ' mr-10 w-32 md:w-44';
+  const dormant = 'hover:opacity-60' + active;
+
   return (
     <>
       <div className="fixed top-0 z-30 flex h-16 w-full flex-row justify-between bg-violet-600 px-9 md:h-24">
         <div className="max-w-400 m-auto flex h-full w-full flex-row items-center justify-between">
           <Link href="/">
-            <div className="mr-10 w-32 md:w-44">
+            <div className={pathname === '/' ? active : dormant}>
               <Image
                 src={logo}
                 alt="Plain monotone version of Reformr text logo."
