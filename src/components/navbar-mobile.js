c63c7f0327afb5adedb8c44f2d5e9a8818a1a574
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-02 00:45:31 +0700

    Fix link text position on mobile nav overlay
    Link text was much too far down on mobile when the nav overlay was open. Caused it to overflow the bottom of the screen.

--- src/components/navbar-mobile.js
+++ src/components/navbar-mobile.js
@@ -2,7 +2,7 @@ import NavLinks from './nav-links';
 
 export default function MobileNavbar({ setOpen }) {
   return (
-    <div className="fixed bottom-0 z-30 flex h-[calc(100vh-48px)] w-full flex-col justify-around bg-violet-500 px-9">
+    <div className="fixed bottom-0 z-30 flex h-[calc(100vh-48px)] w-full flex-col bg-violet-500 px-9">
       <NavLinks setOpen={setOpen} />
     </div>
   );
