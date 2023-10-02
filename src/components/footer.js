6cf6695026abd28a4238683c5bff924f948d0234
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-02 09:28:24 +0700

    Add support section to footer
    A heading and 3 links as with the Squarespace site.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,16 @@
 
 import { AiOutlineInstagram as Instagram } from 'react-icons/ai';
 import Image from 'next/image';
+import Link from 'next/link';
 import { INSTAGRAM } from '@/util/urls';
 import logo from 'public/images/text-logo-coloured.png';
 
 export default function Footer() {
+  const linkStyle = 'hover:text-violet-600 transition';
+
   return (
     <footer className="px-9">
-      <div className="flex flex-row">
+      <div className="flex flex-row text-xl md:text-2xl">
         <section>
           <div className="mb-5 w-64 md:w-80">
             <Image
@@ -24,6 +27,18 @@ export default function Footer() {
             rel="noreferrer"
           >
             <Instagram size={50} className="transition hover:fill-violet-600" />
+          </a>
+        </section>
+        <section className="flex flex-col">
+          <h4 className="font-bold">Support</h4>
+          <Link href="/contact" className={linkStyle}>
+            Contact Us
+          </Link>
+          <Link href="/contact" className={linkStyle}>
+            Customer Support
+          </Link>
+          <a href="/#faq" className={linkStyle}>
+            FAQs
           </a>
         </section>
       </div>
