891e788e1a193d438772e8f2413a994b7c750957
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-02 09:04:34 +0700

    Add the Instagram icon to the footer
    Opens the Reformr Instagram page in a new browser tab when clicked. Changes to purple over 0.3s when clicked.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 'use client';
 
+import { AiOutlineInstagram as Instagram } from 'react-icons/ai';
 import Image from 'next/image';
+import { INSTAGRAM } from '@/util/urls';
 import logo from 'public/images/text-logo-coloured.png';
 
 export default function Footer() {
@@ -8,13 +10,21 @@ export default function Footer() {
     <footer className="px-9">
       <div className="flex flex-row">
         <section>
-          <div className="w-64 md:w-80">
+          <div className="mb-5 w-64 md:w-80">
             <Image
               src={logo}
               alt="Coloured version of Reformr text logo."
               sizes="(max-width: 767px) 16rem, 20rem"
             />
           </div>
+          <a
+            href={INSTAGRAM}
+            aria-label="Instagram"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Instagram size={50} className="transition hover:fill-violet-600" />
+          </a>
         </section>
       </div>
       <p className="my-10 text-center">
