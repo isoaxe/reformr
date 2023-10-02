235ab3eca1468ed3e6d98aa008c1637fbd01d536
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-02 08:06:16 +0700

    Create footer.js
    Just a basic footer with the copyright for now.

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="mb-10 text-center">
+        © {new Date().getFullYear()} Reformr Health LLC. All rights reserved.
+      </p>
+    </footer>
+  );
+}
