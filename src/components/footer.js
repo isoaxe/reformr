72ef88d9fda015c7c1c2ff1ad8f8595de6cbe227
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-02 08:43:01 +0700

    Add reformr text logo to footer
    Nest the HTML in a way that is appropriate for the future layout of the footer element.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,23 @@
+'use client';
+
+import Image from 'next/image';
+import logo from 'public/images/text-logo-coloured.png';
+
 export default function Footer() {
   return (
-    <footer>
-      <p className="mb-10 text-center">
+    <footer className="px-9">
+      <div className="flex flex-row">
+        <section>
+          <div className="w-64 md:w-80">
+            <Image
+              src={logo}
+              alt="Coloured version of Reformr text logo."
+              sizes="(max-width: 767px) 16rem, 20rem"
+            />
+          </div>
+        </section>
+      </div>
+      <p className="my-10 text-center">
         © {new Date().getFullYear()} Reformr Health LLC. All rights reserved.
       </p>
     </footer>
