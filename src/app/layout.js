2c58f29e8b643abccc2019abde4b916b968f31a0
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-03 10:17:52 +0700

    Decrease top margin for layout
    This needs to align with the navbar height which was reduced. Top of page content area should be same as bottom of navbar.

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${montserrat.className} ${work.variable} mt-16 md:mt-24`}
+        className={`${montserrat.className} ${work.variable} mt-12 md:mt-16`}
       >
         <Navbar />
         {children}
