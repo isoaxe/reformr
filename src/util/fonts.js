01f68b55f290ff5c82fc1160a681d467034f9eb1
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-03 15:42:16 +0700

    Create a new local font variable
    This was done in a separate file as there are 16 objects in the src array to import every font file.

---
+++ src/util/fonts.js
@@ -0,0 +1,87 @@
+import localFont from 'next/font/local';
+
+export const sohne = localFont({
+  display: 'swap',
+  src: [
+    {
+      path: '../fonts/söhne/Söhne-Extraleicht.otf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Leicht.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Buch.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Kräftig.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Halbfett.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Dreiviertelfett.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Fett.otf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-Extrafett.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/söhne/Söhne-ExtraleichtKursiv.otf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-LeichtKursiv.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-BuchKursiv.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-KräftigKursiv.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-HalbfettKursiv.otf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-DreiviertelfettKursiv.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-FettKursiv.otf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/söhne/Söhne-ExtrafettKursiv.otf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+});
