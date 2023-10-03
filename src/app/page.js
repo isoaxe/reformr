64b40d5dfe1d05cffe0ea81c39bc06796e1e4804
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-03 10:35:49 +0700

    Add hero video to homepage
    Make the video occupy the remainder of the screen (i.e. everything excluding navbar).

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <h1 className="p-4 text-2xl md:p-8 md:text-4xl">
-        This is the <span className="font-bold text-blue-600">Home</span> page
-      </h1>
+    <main>
+      <section className="relative h-[calc(100vh-3rem)] md:h-[calc(100vh-4rem)]">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute z-0 h-full w-full object-cover"
+          alt="Slow-motion video of a woman slowly exhaling whilst rolling her shoulders."
+        >
+          <source src="videos/home-hero.mp4" type="video/mp4" />
+        </video>
+      </section>
     </main>
   );
 }
