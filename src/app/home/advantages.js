9a0d8480543e96d96e69decf52e75c5d0bf5dd4e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-04 21:09:49 +0700

    Add final section to advantages
    Last section included a bullet point list so a bespoke local component was made for this. Otherwise, it was similar to the previous two sections.

--- src/app/home/advantages.js
+++ src/app/home/advantages.js
@@ -2,9 +2,17 @@ import Image from 'next/image';
 import Button from '@/components/button';
 import vial from '/public/images/vial.jpg';
 import dna from '/public/images/dna.jpg';
+import labLady from '/public/images/lab-lady.png';
 
 /* Three sections of the homepage that comprise the product pitch. */
 export default function Advantages() {
+  const BulletPoint = ({ text }) => (
+    <div className="my-1 flex flex-row items-start lg:my-2">
+      <div className="mr-2 mt-3 aspect-square h-1.5 rounded-full border border-black lg:mt-3.5"></div>
+      <p className="text-lg font-light md:text-xl lg:text-2xl">{text}</p>
+    </div>
+  );
+
   return (
     <>
       <section className="flex flex-col-reverse items-center justify-center bg-sky-200 px-4 py-20 xs:px-9 lg:flex-row lg:py-32">
@@ -56,6 +64,33 @@ export default function Advantages() {
           <Image
             src={dna}
             alt="Stylised graphic of a DNA strand with blue hue."
+            fill
+            className="z-0 rounded-full object-cover"
+            sizes="32rem"
+          />
+        </div>
+      </section>
+      <section className="flex flex-col-reverse items-center justify-center bg-pink-200 px-4 py-20 xs:px-9 lg:flex-row lg:py-32">
+        <div className="mr-8 mt-12 flex max-w-2xl flex-col items-center lg:mt-0 lg:max-w-lg lg:items-start xl:mr-32 2xl:mr-56 2xl:max-w-4xl">
+          <h2 className="mb-8 text-2xl font-bold md:mb-16 md:text-3xl lg:text-4xl">
+            More than just weight loss
+          </h2>
+          <p className="mb-1 text-lg font-light md:text-xl lg:text-2xl">
+            Reformr members can expect a range of potential physical and mental
+            health benefits including:
+          </p>
+          <div className="mb-8 ml-6 self-start md:mb-16 lg:ml-8">
+            <BulletPoint text="Improved metabolic health." />
+            <BulletPoint text="Reduced cardiovascular risk factors like cholesterol." />
+            <BulletPoint text="Reversal of fatty liver." />
+            <BulletPoint text="Improvements in depression, anxiety, and quality of life." />
+          </div>
+          <Button text="Knowledge Centre" link="/blog" bgShade="light" />
+        </div>
+        <div className="relative aspect-square w-full max-w-md rounded-full shadow-2xl lg:max-w-lg">
+          <Image
+            src={labLady}
+            alt="A smiling woman in a lab with her arms crossed."
             fill
             className="z-0 rounded-full object-cover"
             sizes="32rem"
