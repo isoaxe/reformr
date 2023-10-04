f8a4fccda39899affa820dd80619b8ba98ebdd9e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-04 20:29:34 +0700

    Add second section to advantages
    The second of three sections that comprise the sales pitch.

--- src/app/home/advantages.js
+++ src/app/home/advantages.js
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 import Button from '@/components/button';
 import vial from '/public/images/vial.jpg';
+import dna from '/public/images/dna.jpg';
 
 /* Three sections of the homepage that comprise the product pitch. */
 export default function Advantages() {
   return (
     <>
       <section className="flex flex-col-reverse items-center justify-center bg-sky-200 px-4 py-20 xs:px-9 lg:flex-row lg:py-32">
-        <div className="mr-4 mt-12 flex max-w-2xl flex-col items-center lg:mt-0 lg:max-w-lg lg:items-start xl:mr-32 2xl:mr-56 2xl:max-w-4xl">
+        <div className="mr-8 mt-12 flex max-w-2xl flex-col items-center lg:mt-0 lg:max-w-lg lg:items-start xl:mr-32 2xl:mr-56 2xl:max-w-4xl">
           <h2 className="mb-8 text-2xl font-bold md:mb-16 md:text-3xl lg:text-4xl">
-            Experience the next generation of weight loss.
+            Experience the next generation of weight loss
           </h2>
           <p className="mb-4 text-lg font-light md:mb-8 md:text-xl lg:text-2xl">
             Reformr combines state-of-the-art prescription weight loss
@@ -30,6 +31,31 @@ export default function Advantages() {
           <Image
             src={vial}
             alt="Close-up of a vial being held by a lab technician in the background."
+            fill
+            className="z-0 rounded-full object-cover"
+            sizes="32rem"
+          />
+        </div>
+      </section>
+      <section className="flex flex-col-reverse items-center justify-center px-4 py-20 xs:px-9 lg:flex-row-reverse lg:py-32">
+        <div className="ml-8 mt-12 flex max-w-2xl flex-col items-center lg:mt-0 lg:max-w-lg lg:items-start xl:ml-32 2xl:ml-56 2xl:max-w-4xl">
+          <h2 className="mb-8 text-2xl font-bold md:mb-16 md:text-3xl lg:text-4xl">
+            Cutting-edge medications
+          </h2>
+          <p className="mb-8 text-lg font-light md:mb-16 md:text-xl lg:text-2xl">
+            Discover how GLP-1 medications like{' '}
+            <span className="font-semibold">Saxenda (liraglutide)</span> are
+            being used as{' '}
+            <span className="font-semibold">powerful weight loss tools</span>{' '}
+            that are revolutionising how hundreds of thousands of people are
+            losing weight.
+          </p>
+          <Button text="Begin Assessment" link="/quiz" bgShade="light" />
+        </div>
+        <div className="relative aspect-square w-full max-w-md rounded-full shadow-2xl lg:max-w-lg">
+          <Image
+            src={dna}
+            alt="Stylised graphic of a DNA strand with blue hue."
             fill
             className="z-0 rounded-full object-cover"
             sizes="32rem"
