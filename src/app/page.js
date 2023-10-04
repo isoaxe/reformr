4a970bcc62cf143f92311314344bf2c2575b2a90
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-04 15:35:36 +0700

    Implement the advantages component
    Import and implement the component so that it's visible on the homepage.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from './home/hero';
 import Process from './home/process';
+import Advantages from './home/advantages';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Process />
+      <Advantages />
     </main>
   );
 }
