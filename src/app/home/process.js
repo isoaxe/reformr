920677501c573e18f9e0a28178545f75cb4a59d7
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-04 10:14:30 +0700

    Add new process component to homepage
    Just populated with a responsive heading for now.

---
+++ src/app/home/process.js
@@ -0,0 +1,10 @@
+/* Section that outlines the stages in the process shown on the homepage. */
+export default function Process() {
+  return (
+    <section className="my-10 px-4 md:my-28">
+      <h2 className="text-center text-xl font-light md:text-2xl lg:text-3xl">
+        Personalised medical weight loss. One simple subscription.
+      </h2>
+    </section>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from './home/hero';
+import Process from './home/process';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Process />
     </main>
   );
 }
