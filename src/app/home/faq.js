9aa765c36a0122f9ba344b0e43d4778e54cbbc43
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-05 09:33:19 +0700

    Use FAQ data from imported file
    Consume FAQs from the data file created recently. Still just renders a single, mostly unstyled FAQ for now.

--- src/app/home/faq.js
+++ src/app/home/faq.js
@@ -2,13 +2,14 @@
 
 import { useState } from 'react';
 import Collapse from '@mui/material/Collapse';
+import { faqs } from '@/util/data';
 
 /* The Frequently Asked Questions section of the homepage. */
 export default function FAQ() {
   const [checked, setChecked] = useState(false);
 
   /* Single question with animated expansion. */
-  const faq = (question, answer) => (
+  const faq = ({ question, answer }) => (
     <div className="max-w-xl text-lg md:text-xl lg:text-2xl">
       <p onClick={() => setChecked(!checked)}>{question}</p>
       <Collapse in={checked}>
@@ -22,14 +23,5 @@ export default function FAQ() {
     </div>
   );
 
-  return (
-    <section>
-      {faq(
-        'You can read more about Saxenda including how it works, how effective it is, and its side effects here.',
-        [
-          'The Reformr Metabolic Reset Program includes a monthly supply of liraglutide (Saxenda). Saxenda is the only GLP-1 medication approved for the treatment of obesity in New Zealand.',
-        ]
-      )}
-    </section>
-  );
+  return <section>{faq(faqs[0])}</section>;
 }
