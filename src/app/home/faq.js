dc1a648b9a2502a8c57d1dbe988d3965bae5e305
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-05 10:45:28 +0700

    Fix minor FAQ bug where answer won't close
    It was working whereby if a new question was clicked the old one would close. But if an open question was clicked a second time, it would not close. This fixes that issue.
    Also updated the index variable name and added it as a key to stop Node from complaining.

--- src/app/home/faq.js
+++ src/app/home/faq.js
@@ -10,18 +10,18 @@ export default function FAQ() {
   const [isOpen, setOpen] = useState(initialFaqState);
 
   /* Single question with animated expansion. */
-  const faq = ({ question, answer }, index) => (
-    <div className="max-w-xl text-lg md:text-xl lg:text-2xl">
+  const faq = ({ question, answer }, faqIdx) => (
+    <div className="max-w-xl text-lg md:text-xl lg:text-2xl" key={faqIdx}>
       <p
         onClick={() => {
           const updatedState = initialFaqState;
-          updatedState[index] = true;
+          if (!isOpen[faqIdx]) updatedState[faqIdx] = true;
           setOpen(updatedState);
         }}
       >
         {question}
       </p>
-      <Collapse in={isOpen[index]}>
+      <Collapse in={isOpen[faqIdx]}>
         {/* This is the answer that is initially hidden. */}
         {answer.map((paragraph, idx) => (
           <p className="font-light" key={idx}>
