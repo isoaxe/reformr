150b9afbc4b3fb9faf66422fcc80b93d98f61936
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-05 08:56:55 +0700

    Basic working version of Collapse for FAQ
    Just to demo the Material UI component.
    Needed the faq as a function rather than a local sub-component as doing so breaks the smooth expansion animation.

--- src/app/home/faq.js
+++ src/app/home/faq.js
@@ -1,6 +1,29 @@
 'use client';
 
+import { useState } from 'react';
+import Collapse from '@mui/material/Collapse';
+
 /* The Frequently Asked Questions section of the homepage. */
 export default function FAQ() {
-  return <section></section>;
+  const [checked, setChecked] = useState(false);
+
+  /* Single question with animated expansion. */
+  const faq = (question, answer) => (
+    <div className="max-w-xl text-lg md:text-xl lg:text-2xl">
+      <p onClick={() => setChecked(!checked)}>{question}</p>
+      <Collapse in={checked}>
+        {/* This is the answer that is initially hidden. */}
+        <p className="font-light">{answer}</p>
+      </Collapse>
+    </div>
+  );
+
+  return (
+    <section>
+      {faq(
+        'You can read more about Saxenda including how it works, how effective it is, and its side effects here.',
+        'The Reformr Metabolic Reset Program includes a monthly supply of liraglutide (Saxenda). Saxenda is the only GLP-1 medication approved for the treatment of obesity in New Zealand.'
+      )}
+    </section>
+  );
 }
