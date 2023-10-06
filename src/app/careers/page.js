bbbe49a2c6fe6edc8991fef85433596eaee0df12
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-06 09:21:56 +0700

    Add background to careers page
    Add the image of a woman smiling from the previous commit as a background at the top of the careers page.

--- src/app/careers/page.js
+++ src/app/careers/page.js
@@ -1,10 +1,20 @@
+import Image from 'next/image';
+import womanWithPhone from '/public/images/woman-smiling-with-phone.jpeg';
+
 export default function Careers() {
   return (
-    <main className="min-h-screen">
-      <h1 className="p-4 text-2xl md:p-8 md:text-4xl">
-        This is the <span className="font-bold text-blue-600">Careers</span>{' '}
-        page
-      </h1>
+    <main>
+      <section className="relative flex h-[80vh] items-center sm:h-screen md:h-[115vh] lg:h-[130vh] xl:h-[150vh] 2xl:h-[160vh]">
+        <div className="absolute h-full w-full">
+          <Image
+            src={womanWithPhone}
+            alt="A smiling woman looking at her phone with a neon background."
+            fill
+            className="z-0 object-cover"
+            sizes="100vw"
+          />
+        </div>
+      </section>
     </main>
   );
 }
