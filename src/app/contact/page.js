3c5faab260270eebcb0e2fbc30347b7656066972
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-06 21:59:58 +0700

    Add field to select type of help on contact form
    Allow the user to select the type of help required from a dropdown menu.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,42 @@
 'use client';
 
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function Contact() {
+  const [helpType, setHelpType] = useState('');
+
+  /* Dropdown menu that allows user to select type of help required. */
+  function HelpSelect() {
+    return (
+      <Box sx={{ my: 1 }}>
+        <FormControl fullWidth>
+          <InputLabel>How can we help you?</InputLabel>
+          <Select
+            value={helpType}
+            label="How can we help you?"
+            onChange={(e) => setHelpType(e.target.value)}
+          >
+            <MenuItem value={1}>Issue with my order or delivery</MenuItem>
+            <MenuItem value={2}>Medical support</MenuItem>
+            <MenuItem value={3}>Careers at Reformr</MenuItem>
+            <MenuItem value={4}>Media enquiries</MenuItem>
+            <MenuItem value={5}>Learn more about Reformr</MenuItem>
+            <MenuItem value={6}>Following up from consultation</MenuItem>
+            <MenuItem value={7}>Partnering with Reformr</MenuItem>
+            <MenuItem value={8}>Something else</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    );
+  }
+
   return (
     <main>
       <section className="mx-auto my-20 flex max-w-3xl flex-col px-4 xs:px-9 sm:my-28">
@@ -22,6 +55,7 @@ export default function Contact() {
           <TextField label="Last Name" className="w-full sm:w-1/2" />
         </div>
         <TextField label="Email" sx={{ my: 1 }} />
+        <HelpSelect />
         <TextField label="Message" sx={{ mt: 1, mb: 2 }} multiline rows={6} />
         <Button
           variant="contained"
