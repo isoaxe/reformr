d4f91ec57760b72e653880e7d59f5dbbdadfe64b
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-06 21:16:07 +0700

    Add fields to contact form
    Add fields for first name, surname, email and message. Not hooked to state yet or anything. Made responsive though.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,7 @@
+'use client';
+
+import TextField from '@mui/material/TextField';
+
 export default function Contact() {
   return (
     <main>
@@ -5,10 +9,19 @@ export default function Contact() {
         <h1 className="text-4xl font-extrabold xs:text-5xl md:text-6xl">
           Contact Us
         </h1>
-        <p className="my-4 text-lg sm:text-xl md:my-8 md:text-2xl">
+        <p className="my-4 text-lg font-light sm:text-xl md:my-8 md:text-2xl">
           Thanks for getting in touch. Please provide as much information as
           possible and we will get back to you within 2 business days.
         </p>
+        <div className="my-2 flex flex-col sm:flex-row">
+          <TextField label="First Name" className="w-full sm:w-1/2" />
+          <div className="h-4 w-4 sm:w-4">
+            {/* Spacer between Name fields */}
+          </div>
+          <TextField label="Last Name" className="w-full sm:w-1/2" />
+        </div>
+        <TextField label="Email" sx={{ my: 1 }} />
+        <TextField label="Message" sx={{ my: 1 }} multiline rows={6} />
       </section>
     </main>
   );
