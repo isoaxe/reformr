c5e943920e8268fb5beb3e5234b790e4531cee2f
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-06 20:48:13 +0700

    Add heading and paragraph to contact page
    Similar to the Squarespace site but made responsive.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,15 @@
 export default function Contact() {
   return (
-    <main className="min-h-screen">
-      <h1 className="p-4 text-2xl md:p-8 md:text-4xl">
-        This is the <span className="font-bold text-blue-600">Contact</span>{' '}
-        page
-      </h1>
+    <main>
+      <section className="mx-auto my-20 flex max-w-3xl flex-col px-4 xs:px-9 sm:my-28">
+        <h1 className="text-4xl font-extrabold xs:text-5xl md:text-6xl">
+          Contact Us
+        </h1>
+        <p className="my-4 text-lg sm:text-xl md:my-8 md:text-2xl">
+          Thanks for getting in touch. Please provide as much information as
+          possible and we will get back to you within 2 business days.
+        </p>
+      </section>
     </main>
   );
 }
