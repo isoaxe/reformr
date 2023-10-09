529940264e8407a6fd0e7ecc134b14be57e3cb3f
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-09 12:07:20 +0700

    Fix quiz-button clickable area
    Wrapping the button in the link caused the entire width of the container to be clickable. This makes it so that just the button UI is clickable again.

--- src/components/quiz-button.js
+++ src/components/quiz-button.js
@@ -4,8 +4,8 @@ import { Button } from '@mui/material';
 /* Material UI button used in the screening and medical quizzes. */
 export default function QuizButton({ text, link }) {
   return (
-    <Link href={link}>
-      <Button variant="outlined" className="w-fit text-lg md:text-xl">
+    <Link href={link} className="w-fit">
+      <Button variant="outlined" className="text-lg md:text-xl">
         {text}
       </Button>
     </Link>
