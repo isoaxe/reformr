81efd08dc7715e122017bc8c712d2d6197f6abbc
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-09 10:41:38 +0700

    Make first page of screening quiz more responsive
    Arrange content in a column for mobile to make better use of space. Tweak padding breakpoints etc.

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -6,8 +6,8 @@ import { Button } from '@mui/material';
 export default function Quiz() {
   return (
     <main className="flex min-h-screen flex-col justify-center">
-      <section className="mx-auto flex w-full max-w-6xl flex-row">
-        <div className="relative aspect-square w-1/2 px-4 xs:px-9">
+      <section className="mx-auto flex w-full max-w-md flex-col sm:max-w-6xl sm:flex-row">
+        <div className="relative aspect-square sm:w-1/2">
           <Image
             src={peaceSign}
             alt="A hand making a peace sign with a yellow background."
@@ -16,7 +16,7 @@ export default function Quiz() {
             sizes="24rem"
           />
         </div>
-        <div className="flex w-1/2 flex-col justify-center px-4 text-xl xs:px-9 md:text-2xl xl:text-3xl">
+        <div className="flex aspect-square flex-col justify-center px-4 text-xl sm:w-1/2 md:px-9 md:text-2xl xl:text-3xl">
           <p>Kia Ora!</p>
           <p className="my-6">
             Answer a few simple questions to see if you&apos;re eligible for the
