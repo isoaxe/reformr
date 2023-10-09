9b4b24ae3df1c834b46e4d65ae2922c83ee6cb1f
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-09 10:30:02 +0700

    Add text and button to first quiz page
    Make text and button sizes responsive too.

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import peaceSign from '/public/images/hand-peace-sign.png';
+import { Button } from '@mui/material';
 
 /* This is just the first intro page of the quiz and doen't contain questions. */
 export default function Quiz() {
@@ -14,6 +15,16 @@ export default function Quiz() {
             className="z-0 object-cover"
             sizes="24rem"
           />
+        </div>
+        <div className="flex w-1/2 flex-col justify-center px-4 text-xl xs:px-9 md:text-2xl xl:text-3xl">
+          <p>Kia Ora!</p>
+          <p className="my-6">
+            Answer a few simple questions to see if you&apos;re eligible for the
+            Metabolic Reset Program.
+          </p>
+          <Button variant="outlined" className="w-fit text-lg md:text-xl">
+            Start
+          </Button>
         </div>
       </section>
     </main>
