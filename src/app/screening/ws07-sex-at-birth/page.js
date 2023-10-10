d4aa91b155cbb218650a8c25abfec57a99d53fb7
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-10 17:20:37 +0700

    Add styles to sex-at-birth radio labels
    Allows the styling of the text which was far too small.
    Also correct question tense.

--- src/app/screening/ws07-sex-at-birth/page.js
+++ src/app/screening/ws07-sex-at-birth/page.js
@@ -4,16 +4,23 @@ import { useState } from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Typography from '@mui/material/Typography';
 import Button from '@/components/quiz/button';
 
 /* Collect the users sex as recorded at birth. */
 export default function Sex() {
   const [sex, setSex] = useState('');
 
+  const FormLabel = ({ label }) => (
+    <Typography className="mt-1 text-lg md:text-xl xl:text-2xl">
+      {label}
+    </Typography>
+  );
+
   return (
     <main className="mx-auto flex flex-col">
       <p className="mb-6">
-        What is your <span className="font-semibold">sex at birth</span>?
+        What was your <span className="font-semibold">sex at birth</span>?
       </p>
       <RadioGroup
         aria-labelledby="choose-sex-at-birth-label"
@@ -22,12 +29,20 @@ export default function Sex() {
         onChange={(e) => setSex(e.target.value)}
         sx={{ mb: 3, width: 'fit-content' }}
       >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel
+          value="female"
+          control={<Radio />}
+          label={<FormLabel label="Female" />}
+        />
+        <FormControlLabel
+          value="male"
+          control={<Radio />}
+          label={<FormLabel label="Male" />}
+        />
         <FormControlLabel
           value="intersex"
           control={<Radio />}
-          label="Intersex"
+          label={<FormLabel label="Intersex" />}
         />
       </RadioGroup>
       <Button text="Ok" link="/screening/ws08-height" />
