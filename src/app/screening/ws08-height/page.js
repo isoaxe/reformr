a6c3028c3eb063f6a1a4289976cf1309fd9152ed
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-10 22:58:08 +0700

    Add height page to screening quiz
    A number input to collect the users height. Will only accept an integer. Saves to state but not to database yet.

---
+++ src/app/screening/ws08-height/page.js
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+import Button from '@/components/quiz/button';
+import NumberInput from '@/components/quiz/number-input';
+
+/* Collect height of the user in cm as an integer. */
+export default function Height() {
+  const [height, setHeight] = useState('');
+
+  return (
+    <main className="mx-auto flex flex-col">
+      <p className="mb-6">
+        What is your <span className="font-semibold">height</span> in
+        centimetres (cm)?
+      </p>
+      <NumberInput number={height} setNumber={setHeight} placeholder={'165'} />
+      <Button text="Ok" link="/screening/ws09-weight" />
+    </main>
+  );
+}
