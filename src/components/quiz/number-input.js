6eaaeb09f4addc0137f9165adcdea2c464e3ae98
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-10 22:49:26 +0700

    Create NumberInput component for quizzes
    Standardised number input component. Actually takes text input but disallows non-numeric characters.

---
+++ src/components/quiz/number-input.js
@@ -0,0 +1,24 @@
+import { TextField } from '@mui/material';
+
+/* Just a standardised number input component for the screening and medical quizzes. */
+/* IMPORTANT: All props take a string, not a number! */
+export default function NumberInput({ number, setNumber, placeholder = '' }) {
+  /* Allow only number input (albeit in string form). */
+  const handleChange = (e) => {
+    const regex = /^[0-9\b]+$/;
+    if (e.target.value === '' || regex.test(e.target.value)) {
+      setNumber(e.target.value);
+    }
+  };
+
+  return (
+    <TextField
+      variant="standard"
+      placeholder={placeholder}
+      value={number}
+      onChange={handleChange}
+      sx={{ mb: 3 }}
+      InputProps={{ className: 'text-xl md:text-2xl xl:text-3xl' }}
+    />
+  );
+}
