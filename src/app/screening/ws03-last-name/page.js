8c9a0b49053f55786c2c49fac5b89ddf42e93104
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-10 08:55:45 +0700

    Add email screening quiz page
    An input element to collect the users email. Saves to state but not to database yet.
    Also correct the sentence structure of the previous page (last-name).

--- src/app/screening/ws03-last-name/page.js
+++ src/app/screening/ws03-last-name/page.js
@@ -11,7 +11,7 @@ export default function LastName() {
   return (
     <main className="mx-auto flex flex-col">
       <p className="mb-6">
-        And your <span className="font-semibold">last name?</span>.
+        And your <span className="font-semibold">last name</span>?
       </p>
       <TextInput text={lastName} setText={setLastName} />
       <Button text="Ok" link="/screening/ws04-email" />
