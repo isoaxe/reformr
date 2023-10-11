bf5d4205029201901e98a4745beecd74fbf89a83
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-11 12:47:33 +0700

    Save state to cookie on button click
    When completing a quiz, save the data that the user has just entered to state each time next question is clicked.

--- src/components/quiz/button.js
+++ src/components/quiz/button.js
@@ -1,11 +1,27 @@
+'use client';
+
 import Link from 'next/link';
 import { Button as MuiButton } from '@mui/material';
+import { useCookies } from 'next-client-cookies';
 
 /* Material UI button used in the screening and medical quizzes. */
-export default function Button({ text, link }) {
+export default function Button({ text, link, state }) {
+  const cookies = useCookies();
+
+  const setCookie = () => {
+    if (!state) return; // exit early as no cookie to set
+
+    const key = Object.keys(state)[0]; // get name of state, e.g. 'firstName'
+    cookies.set(key, state[key]);
+  };
+
   return (
     <Link href={link} className="w-fit">
-      <MuiButton variant="outlined" className="text-lg md:text-xl">
+      <MuiButton
+        variant="outlined"
+        className="text-lg md:text-xl"
+        onClick={setCookie}
+      >
         {text}
       </MuiButton>
     </Link>
