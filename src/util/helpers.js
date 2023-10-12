07cca6aab9fd89c67602cf2c0ebca10818b077cf
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-12 22:43:32 +0700

    Remove sanitiseString from helpers.js
    This is not actually necessary. It's fine to have non-latin characters in the document name (such as apostrophes).

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -4,23 +4,10 @@
 
 /* Creates a unique document ID for Firestore. */
 export function createDocumentId(name) {
-  const sanitisedName = sanitiseString(name);
   const number = Math.floor(Math.random() * 1000000);
-  return `${sanitisedName}-${number}`;
+  return `${name}-${number}`;
 }
 
 /*
  * Helpers for the helper functions. Only used within helpers.js and not exported.
  */
-
-/* Takes a string and removes any non-latin characters except hyphens. */
-function sanitiseString(string) {
-  const lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const alphabet = uppercaseAlphabet + lowercaseAlphabet + '-';
-  let sanitisedString = '';
-  for (const char of string) {
-    if (alphabet.includes(char)) sanitisedString += char;
-  }
-  return sanitisedString;
-}
