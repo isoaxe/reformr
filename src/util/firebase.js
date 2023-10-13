ca6010eafda18c5bcae09ee17d203775fb5ee41c
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-13 12:44:15 +0700

    Suspend analytics from Firebase
    Can comment out as not required for v2. Was causing an issue when the 'weight' page of the screening quiz was reloaded (unsupported environment error). Did not occur when first navigating to page though.

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
@@ -15,6 +15,9 @@ const firebaseConfig = {
 
 // Initialise Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+// TODO: Add analytics when required, current implementation was causing issues.
+// Search commit 'Suspend analytics from Firebase' for more details.
