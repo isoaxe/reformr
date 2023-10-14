33e99b5841526abbe8412944cdc1afd7165a9f4c
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-14 22:03:48 +0700

    Ensure that docId always contains a 6-digit number
    Was losing the leading zeros before. This ensures that we keep them which may be helpful when parsing IDs later.

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -2,10 +2,14 @@
  * Helper functions available for use throughout the project.
  */
 
-/* Creates a unique document ID for Firestore. */
+/* Creates a unique document ID for Firestore. Number always 6 digits long. */
 export function createDocId(name) {
-  const number = Math.floor(Math.random() * 1000000);
-  return `${name}-${number}`;
+  const idNumber = Math.floor(Math.random() * 1000000);
+  let idString = idNumber.toString(); // possibly < 6 digits
+  const leadingZeros = 6 - idString.length;
+  const leadingZerosAsString = '0'.repeat(leadingZeros);
+  idString = leadingZerosAsString + idString; // always 6 digits
+  return `${name}-${idString}`;
 }
 
 /*
