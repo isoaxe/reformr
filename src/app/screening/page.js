5a6787ee8430bca263d4b84159906980b94cb3c6
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-17 15:51:24 +0700

    Always validate first two pages of screening quiz
    These aren't questions and the user just needs to click to proceed. No data collected thus none to check.

--- src/app/screening/page.js
+++ src/app/screening/page.js
@@ -21,7 +21,11 @@ export default function Screening() {
           Answer a few simple questions to see if you&apos;re eligible for the
           <span className="font-semibold"> Metabolic Reset Program.</span>
         </p>
-        <Button text="Start" link="/screening/ws01-accept-terms" />
+        <Button
+          text="Start"
+          link="/screening/ws01-accept-terms"
+          disabled={false}
+        />
       </div>
     </main>
   );
