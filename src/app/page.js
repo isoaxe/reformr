944bdb147ef622fc6f71b8b21cc02223262af2ec
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-17 13:25:15 +0700

    Update comment for Entry page
    A better description of for why Entry has replaced Home.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 /* This replaces <Home/> as the entry point to the app. */
-/* Don't want a default layout but do want one for Home.  */
+/* Needed to move <Home/> so that new layout got applied to it. */
 export default function Entry() {
   redirect('/main/home ');
 }
