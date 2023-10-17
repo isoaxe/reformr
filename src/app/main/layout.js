7474186a66551aae8af0f59e5fde8a64352475eb
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-17 12:18:25 +0700

    Update full-navbar name where used
    Forgot to update the import statement for the renamed FullNavbar. Was still working as it's a default import and the path is correct. However, better to match the exported functions name.

--- src/app/main/layout.js
+++ src/app/main/layout.js
@@ -1,10 +1,10 @@
-import Navbar from '@/components/layout/full-navbar';
+import FullNavbar from '@/components/layout/full-navbar';
 import Footer from '@/components/layout/footer';
 
 export default function MainLayout({ children }) {
   return (
     <div>
-      <Navbar />
+      <FullNavbar />
       {children}
       <Footer />
     </div>
