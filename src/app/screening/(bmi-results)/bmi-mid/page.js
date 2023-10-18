32514f87cb23bb5bf4a58dae34f9ccfaba67f9e3
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-18 15:13:41 +0700

    Add page for mid BMI result (25-30)
    Same format as the intro screen for the quiz. Even using the same image for now until requirements for page have been clarified.
    Button will have a 404 for now as route does not lead anywhere.

---
+++ src/app/screening/(bmi-results)/bmi-mid/page.js
@@ -0,0 +1,34 @@
+'use client';
+
+import { useCookies } from 'next-client-cookies';
+import Image from 'next/image';
+import Button from '@/components/quiz/button';
+import peaceSign from '/public/images/hand-peace-sign.png';
+
+/* User will be asked some medical questions to determine eligibility. */
+export default function MidBmi() {
+  const cookies = useCookies();
+
+  return (
+    <main className="flex flex-col sm:flex-row">
+      <div className="relative aspect-square sm:w-1/2">
+        <Image
+          src={peaceSign}
+          alt="A hand making a peace sign with a yellow background."
+          fill
+          className="z-0 object-cover"
+          sizes="24rem"
+        />
+      </div>
+      <div className="flex aspect-square flex-col justify-center sm:w-1/2 sm:px-9">
+        <p>Thanks for taking the time to complete this quiz.</p>
+        <p className="my-6">
+          You have a BMI of {cookies.get('bmi')}, so we will need to ask some
+          additional questions to determine your eligibility for the Reformr
+          weight loss program.
+        </p>
+        <Button text="More Questions" link="/medical" />
+      </div>
+    </main>
+  );
+}
