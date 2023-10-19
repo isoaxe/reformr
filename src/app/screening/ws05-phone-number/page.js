e597d7cdbe721f05955ce946681e04b590cbf8b9
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-19 13:13:00 +0700

    Display toast on next screen if account exists
    If there's already an account using the email the user has entered, display it here on the following question page.
    Was intending to display this on the BMI page following the screening quiz, but makes much more sense to do it here.

--- src/app/screening/ws05-phone-number/page.js
+++ src/app/screening/ws05-phone-number/page.js
@@ -2,13 +2,17 @@
 
 import { useState, useEffect } from 'react';
 import { MuiTelInput, matchIsValidTel } from 'mui-tel-input';
+import { useCookies } from 'next-client-cookies';
 import Button from '@/components/quiz/button';
+import Toast from '@/components/toast';
 import { useCookieState } from '@/util/hooks';
 
 /* Collect users mobile phone number. */
 export default function PhoneNumber() {
   const [phone, setPhone] = useState('');
   const [isDisabled, setDisabled] = useState(true);
+  const [showToast, setShowToast] = useState(false);
+  const cookies = useCookies();
 
   useCookieState('screening', 'phone', setPhone);
 
@@ -18,6 +22,10 @@ export default function PhoneNumber() {
     if (matchIsValidTel(phone)) setDisabled(false);
     else setDisabled(true);
   }, [phone]);
+
+  useEffect(() => {
+    if (cookies.get('isAccountCreated') === 'true') setShowToast(true);
+  }, [cookies]);
 
   return (
     <main className="mx-auto flex flex-col">
@@ -43,6 +51,12 @@ export default function PhoneNumber() {
         state={{ phone }}
         isDisabled={isDisabled}
         quiz="screening"
+      />
+      <Toast
+        message="An account for this email has already been created"
+        severity="warning"
+        open={showToast}
+        setOpen={setShowToast}
       />
     </main>
   );
