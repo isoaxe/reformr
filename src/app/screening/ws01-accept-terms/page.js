32e3cc9f62c89534a74c1b01fbae465af388a0ad
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-20 23:13:11 +0700

    Refactor reCaptcha in separate component
    Extract all of the reCAPTCHA client-side logic into a separate component and implement it.

--- src/app/screening/ws01-accept-terms/page.js
+++ src/app/screening/ws01-accept-terms/page.js
@@ -1,33 +1,13 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
+import { useState } from 'react';
 import Link from 'next/link';
 import Button from '@/components/quiz/button';
+import Captcha from '@/components/captcha';
 
 /* Accept Reformr terms and conditions. */
 export default function AcceptTerms() {
-  const [token, setToken] = useState(null);
   const [isVerified, setVerified] = useState(false);
-  const captchaRef = useRef(null);
-
-  function handleChange(value) {
-    if (value) setToken(value);
-  }
-
-  useEffect(() => {
-    setTimeout(() => captchaRef.current.execute(), 1000);
-  }, [captchaRef]);
-
-  useEffect(() => {
-    if (token) {
-      (async () => {
-        const res = await fetch(`/api/captcha?token=${token}`);
-        const json = await res.json();
-        if (json.success) setVerified(true);
-      })();
-    }
-  }, [token]);
 
   return (
     <main className="mx-auto max-w-4xl">
@@ -51,13 +31,7 @@ export default function AcceptTerms() {
         .
       </p>
       <Button text="Ok" link="/screening/ws02-first-name" />
-      <ReCAPTCHA
-        ref={captchaRef}
-        size="invisible"
-        badge="bottomright"
-        sitekey="6LdcGLYoAAAAALiS3p1uTXU5979fEhDbF-yyhNda"
-        onChange={handleChange}
-      />
+      <Captcha setVerified={setVerified} />
     </main>
   );
 }
---
+++ src/components/captcha.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, useRef } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+export default function Captcha({ setVerified }) {
+  const [token, setToken] = useState(null);
+  const captchaRef = useRef(null);
+
+  function handleChange(value) {
+    if (value) setToken(value);
+  }
+
+  useEffect(() => {
+    setTimeout(() => captchaRef.current.execute(), 1000);
+  }, [captchaRef]);
+
+  useEffect(() => {
+    if (token) {
+      (async () => {
+        const res = await fetch(`/api/captcha?token=${token}`);
+        const json = await res.json();
+        if (json.success) setVerified(true);
+      })();
+    }
+  }, [token, setVerified]);
+
+  return (
+    <ReCAPTCHA
+      ref={captchaRef}
+      size="invisible"
+      badge="bottomright"
+      sitekey="6LdcGLYoAAAAALiS3p1uTXU5979fEhDbF-yyhNda"
+      onChange={handleChange}
+    />
+  );
+}
