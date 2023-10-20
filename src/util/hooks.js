644dd81d3423bffdeaf6e7ca05245e19affb373d
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-20 09:52:08 +0700

    Create a useFreshCookie hook
    Continually looks for a new cookie every 0.1s. Saves to parent state and stops once found.

--- src/util/hooks.js
+++ src/util/hooks.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useCookies } from 'next-client-cookies';
 
 /* Checks if a provided key is on the cookie and saves to state if so. */
@@ -12,4 +12,24 @@ export function useCookieState(quiz, stateName, setState) {
     const existingState = quizCookie[stateName];
     if (existingState) setState(existingState);
   }, [cookies, quiz, stateName, setState]);
+}
+
+/* Keep checking for a fresh cookie and stop when found. */
+export function useFreshCookie(stateName, setState) {
+  const [staleCookie, setStaleCookie] = useState(true);
+  const cookies = useCookies();
+
+  useEffect(() => {
+    if (staleCookie) {
+      const oldState = cookies.get(stateName);
+      const interval = setInterval(() => {
+        const newState = cookies.get(stateName);
+        if (oldState !== newState) {
+          setState(newState);
+          setStaleCookie(false);
+        }
+      }, 100);
+      return () => clearInterval(interval);
+    }
+  }, [stateName, setState, cookies, staleCookie]);
 }
