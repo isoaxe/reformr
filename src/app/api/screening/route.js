9d3355fcb0814b379fd4d306d501083a3a5aac7b
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-22 16:07:33 +0700

    Move saving of screening data to backend
    Refactor by creating a new API route and moving the logic that saves the screening data to Firestore there.

---
+++ src/app/api/screening/route.js
@@ -0,0 +1,32 @@
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { NextResponse } from 'next/server';
+import { db } from '@/util/firebase';
+
+/* Save screening data to Firestore if token is valid. */
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const bmi = searchParams.get('bmi');
+  const cookieAsString = searchParams.get('cookie');
+  const cookieAsJson = JSON.parse(cookieAsString);
+
+  try {
+    /* Get docId from Firestore. */
+    const { email } = cookieAsJson;
+    const emailsRef = doc(db, 'emails', email);
+    const emailSnap = await getDoc(emailsRef);
+    const emailsData = emailSnap.data();
+    const { docId } = emailsData; // get docId from Firestore
+
+    cookieAsJson.bmi = bmi;
+    cookieAsJson.dateCreated = new Date();
+    /* Fine to overwrite data saved to Firestore before account creation. */
+    await setDoc(doc(db, 'users', docId), {
+      screening: cookieAsJson,
+      dateAccountCreated: null,
+    });
+  } catch (err) {
+    console.error('Error saving screening data: ', err);
+  }
+
+  return NextResponse.json({ success: true });
+}
