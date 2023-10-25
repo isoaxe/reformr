11e3b70e0f7cf687ca57e1cdc4fc458ec311b208
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-25 20:17:43 +0700

    Change password field type
    Convert from OutlinedInput to TextField so a standard style can be used that matches other input  fields.

--- src/components/quiz/password.js
+++ src/components/quiz/password.js
@@ -1,28 +1,31 @@
 import { useState } from 'react';
-import { OutlinedInput, InputAdornment, IconButton } from '@mui/material';
+import { TextField, InputAdornment, IconButton } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 export default function Password({ password, setPassword }) {
   const [isVisible, setVisible] = useState(false);
 
   return (
-    <OutlinedInput
+    <TextField
       id="password-field"
       label="Password"
+      variant="standard"
       value={password}
       type={isVisible ? 'text' : 'password'}
       onChange={(e) => setPassword(e.target.value)}
-      endAdornment={
-        <InputAdornment position="end">
-          <IconButton
-            aria-label="toggle password visibility"
-            onClick={() => setVisible(!isVisible)}
-            edge="end"
-          >
-            {isVisible ? <VisibilityOff /> : <Visibility />}
-          </IconButton>
-        </InputAdornment>
-      }
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={() => setVisible(!isVisible)}
+              edge="end"
+            >
+              {isVisible ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
     />
   );
 }
