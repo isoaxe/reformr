9f69da77f85080bbd7776e57f9873bf23111674c
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-25 20:07:45 +0700

    Implement password on account creation page
    Pass value state and setter as props to make it a controlled component.

--- src/app/signup/create-account/page.js
+++ src/app/signup/create-account/page.js
@@ -2,11 +2,13 @@
 
 import { useState } from 'react';
 import { TextField } from '@mui/material';
+import Password from '@/components/quiz/password';
 import { useCookieState } from '@/util/hooks';
 
 /* Immutable email field and password field for account creation.  */
 export default function CreateAccount() {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   useCookieState('screening', 'email', setEmail);
 
@@ -19,6 +21,7 @@ export default function CreateAccount() {
         sx={{ mb: 3 }}
         InputProps={{ className: 'text-xl md:text-2xl xl:text-3xl' }}
       />
+      <Password password={password} setPassword={setPassword} />
     </main>
   );
 }
