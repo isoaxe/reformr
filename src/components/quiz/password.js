fbd7720786bb41b2d8f46494a72e9f6c556fb5d8
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-26 08:46:01 +0700

    Add helper text to password component
    Display helper text below password field. Validates all passwords above 11 characters long. Updates validation / error status as the user types.
    Might develop into a more sophisticated password validator later.

--- src/components/quiz/password.js
+++ src/components/quiz/password.js
@@ -1,31 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
+import { FormHelperText } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 export default function Password({ password, setPassword }) {
   const [isVisible, setVisible] = useState(false);
+  const [helperText, setHelperText] = useState('');
+
+  /* Display password validation in DOM as user types. */
+  useEffect(() => {
+    if (password && password.length < 12)
+      setHelperText('Needs to be > 11 characters');
+    else setHelperText('');
+  }, [password]);
 
   return (
-    <TextField
-      id="password-field"
-      variant="standard"
-      value={password}
-      type={isVisible ? 'text' : 'password'}
-      onChange={(e) => setPassword(e.target.value)}
-      InputProps={{
-        className: 'text-xl md:text-2xl xl:text-3xl',
-        endAdornment: (
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={() => setVisible(!isVisible)}
-              edge="end"
-            >
-              {isVisible ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        ),
-      }}
-    />
+    <>
+      <TextField
+        id="password-field"
+        variant="standard"
+        value={password}
+        type={isVisible ? 'text' : 'password'}
+        onChange={(e) => setPassword(e.target.value)}
+        error={!!helperText}
+        InputProps={{
+          className: 'text-xl md:text-2xl xl:text-3xl',
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setVisible(!isVisible)}
+                edge="end"
+              >
+                {isVisible ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <FormHelperText error={!!helperText}>{helperText}</FormHelperText>
+    </>
   );
 }
