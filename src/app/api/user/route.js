f95227ffce55e22c8bdb4aedebf5bdb45c207183
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-27 17:39:19 +0700

    Verify the token is legit when creating user
    Check the token provided by the user via request params with the one stored in Firebase at the time the reCAPTCHA was solved.
    Send an error response if there isn't a match.

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,18 +1,29 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
 import { NextResponse } from 'next/server';
 import { auth } from '@/util/firebase';
+import { db } from '@/util/firebase';
 
 /* Save screening data to Firestore if token is valid. */
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const email = searchParams.get('email');
   const password = searchParams.get('password');
+  const token = searchParams.get('token');
 
-  let user = null;
   let success = false;
+  let error = '';
   try {
+    /* Get reCAPTCHA token from Firestore. */
+    const captchasRef = doc(db, 'captchas', email);
+    const captchaSnap = await getDoc(captchasRef);
+    const captchasData = captchaSnap.data();
+    const savedToken = captchasData.token;
+    if (token !== savedToken || token.length !== 50)
+      return NextResponse.json({ success, error: 'Invalid reCAPTCHA token' });
+
     const res = await createUserWithEmailAndPassword(auth, email, password);
-    user = res.user;
+    const user = res.user;
     if (user) success = true;
   } catch (err) {
     console.error('Error creating new user: ', err);
