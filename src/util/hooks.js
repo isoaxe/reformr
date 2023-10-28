9985d1cd4f34f8e5559ac09f5c3dda2176b5dab3
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-28 16:15:49 +0700

    Remove redundant Firebase import from hooks
    This Firebase method was removed previously but the import statement wasn't.

--- src/util/hooks.js
+++ src/util/hooks.js
@@ -5,7 +5,6 @@ import {
   signOut,
   sendPasswordResetEmail,
   confirmPasswordReset,
-  createUserWithEmailAndPassword,
   onAuthStateChanged,
 } from 'firebase/auth';
 import { authContext } from './context';
