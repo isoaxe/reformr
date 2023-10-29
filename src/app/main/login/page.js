5c9d98e965da333d4b7e8cf7be52fc2b21a5b722
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-29 10:03:30 +0700

    Build basic elements of the login page
    Has a email and password fields with a submit button. Very similar to the create-account page.
    All of the UI elements are in place with state and error handling.

--- src/app/main/login/page.js
+++ src/app/main/login/page.js
@@ -1,9 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import Password from '@/components/quiz/password';
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [helperText, setHelperText] = useState('');
+
+  async function login() {
+    return null;
+  }
+
   return (
-    <main className="min-h-screen">
-      <h1 className="p-4 text-2xl md:p-8 md:text-4xl">
-        This is the <span className="font-bold text-blue-600">Login</span> page
-      </h1>
+    <main className="flex min-h-[calc(100vh-23rem)] items-center">
+      <div className="mx-auto flex w-full max-w-md flex-col px-4 py-28 xs:px-9 sm:max-w-lg">
+        <TextField
+          variant="standard"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{ mb: 8 }}
+          InputProps={{ className: 'text-xl md:text-2xl xl:text-3xl' }}
+        />
+        <Password
+          password={password}
+          setPassword={setPassword}
+          helperText={helperText}
+          setHelperText={setHelperText}
+        />
+        <Button
+          variant="outlined"
+          className="mt-8 w-fit text-lg md:text-xl"
+          onClick={login}
+          disabled={!password || !!helperText}
+        >
+          Login
+        </Button>
+      </div>
     </main>
   );
 }
