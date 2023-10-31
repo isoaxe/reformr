88c65b5b6ed72fe80a9585d0d41e192d34f90793
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-10-31 22:11:09 +0700

    Refactor wm04 with radio-list component
    Can use RadioList to implement the question for this page in a standardised manner.

--- src/app/signup/medical/wm04-freq-loss-of-control/page.js
+++ src/app/signup/medical/wm04-freq-loss-of-control/page.js
@@ -1,55 +1,26 @@
-'use client';
+import RadioList from '@/components/quiz/radio-list';
 
-import { useState } from 'react';
-import { Radio, RadioGroup, Typography, FormControlLabel } from '@mui/material';
-import Button from '@/components/quiz/button';
-import { useCookieState } from '@/util/hooks';
-
-/* Multiple choice question with a single answer. */
 export default function FreqLossOfControl() {
-  const [answer, setAnswer] = useState('');
-
-  useCookieState('medical', 'wm04_freq_loss_of_control', setAnswer);
-
-  function RadioOption({ label }) {
-    return (
-      <FormControlLabel
-        value={label}
-        control={<Radio />}
-        label={
-          <Typography className="mt-1 text-lg md:text-xl xl:text-2xl">
-            {label}
-          </Typography>
-        }
-      />
-    );
-  }
+  const heading = [
+    'Do you ever feel like you have',
+    'lost control over what you eat?',
+    '',
+  ];
+  const answerLabels = [
+    'All the time',
+    'Every day',
+    'Most days',
+    'Occasionally',
+    'Rarely',
+    'Never',
+  ];
 
   return (
-    <main className="mx-auto flex max-w-2xl flex-col">
-      <p className="mb-6">
-        Do you ever feel like you have
-        <span className="font-semibold"> lost control over what you eat</span>?
-      </p>
-      <RadioGroup
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        sx={{ mb: 3, width: 'fit-content' }}
-      >
-        <RadioOption label="All the time" />
-        <RadioOption label="Every day" />
-        <RadioOption label="Most days" />
-        <RadioOption label="Occasionally" />
-        <RadioOption label="Rarely" />
-        <RadioOption label="Never" />
-      </RadioGroup>
-      <Button
-        text="Ok"
-        link="./wm05-extent-of-food-domination"
-        state={{ wm04_freq_loss_of_control: answer }}
-        isDisabled={!answer}
-        quiz="medical"
-      />
-    </main>
+    <RadioList
+      heading={heading}
+      questionId="wm04_freq_loss_of_control"
+      answerLabels={answerLabels}
+      nextPage="wm05-extent-of-food-domination"
+    />
   );
 }
