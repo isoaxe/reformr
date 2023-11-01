97d95d6bae5f80505bd4119f5a96de32e7c5a2f7
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-01 14:36:31 +0700

    Build question wm13 with ChooseMultiple
    Use the new standardised component so we only need to pass in the data that changes page-to-page.

---
+++ src/app/signup/medical/wm13-thyroid-activity/page.js
@@ -0,0 +1,23 @@
+import ChooseMultiple from '@/components/quiz/choose-multiple';
+
+export default function ThyroidActivity() {
+  const heading = [
+    'Have you ever been',
+    'diagnosed',
+    'with either of the following?',
+  ];
+  const answerLabels = [
+    'Hyperthyroidism (overactive thyroid)',
+    'Hypothyroidism (underactive thyroid)',
+    'Neither of these',
+  ];
+
+  return (
+    <ChooseMultiple
+      heading={heading}
+      questionId="wm13_thyroid_activity"
+      answerLabels={answerLabels}
+      nextPage="health-and-treatment"
+    />
+  );
+}
