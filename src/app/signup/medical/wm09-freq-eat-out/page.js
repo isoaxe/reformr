df54a942929f8d9102947647f1acc8dfb7cfd388
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-01 09:30:25 +0700

    Refactor wm09 with radio-list component
    Can use RadioList to implement the question for this page in a standardised manner.

--- src/app/signup/medical/wm09-freq-eat-out/page.js
+++ src/app/signup/medical/wm09-freq-eat-out/page.js
@@ -1,54 +1,21 @@
-'use client';
+import RadioList from '@/components/quiz/radio-list';
 
-import { useState } from 'react';
-import { Radio, RadioGroup, Typography, FormControlLabel } from '@mui/material';
-import Button from '@/components/quiz/button';
-import { useCookieState } from '@/util/hooks';
-
-/* Multiple choice question with a single answer. */
 export default function FreqEatOut() {
-  const [answer, setAnswer] = useState('');
-
-  useCookieState('medical', 'wm09_freq_eat_out', setAnswer);
-
-  function RadioOption({ label }) {
-    return (
-      <FormControlLabel
-        value={label}
-        control={<Radio />}
-        label={
-          <Typography className="mt-1 text-lg md:text-xl xl:text-2xl">
-            {label}
-          </Typography>
-        }
-      />
-    );
-  }
+  const heading = ['How many times a week do you', 'eat out or order in?', ''];
+  const answerLabels = [
+    'Multiple times a day',
+    'At least once a day',
+    'Between 4-6 days',
+    'Between 2-3 days',
+    'Once or less',
+  ];
 
   return (
-    <main className="mx-auto flex max-w-2xl flex-col">
-      <p className="mb-6">
-        How many times a week do you
-        <span className="font-semibold"> eat out or order in</span>?
-      </p>
-      <RadioGroup
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        sx={{ mb: 3, width: 'fit-content' }}
-      >
-        <RadioOption label="Multiple times a day" />
-        <RadioOption label="At least once a day" />
-        <RadioOption label="Between 4-6 days" />
-        <RadioOption label="Between 2-3 days" />
-        <RadioOption label="Once or less" />
-      </RadioGroup>
-      <Button
-        text="Ok"
-        link="./wm10-physical-fitness"
-        state={{ wm09_freq_eat_out: answer }}
-        isDisabled={!answer}
-        quiz="medical"
-      />
-    </main>
+    <RadioList
+      heading={heading}
+      questionId="wm09_freq_eat_out"
+      answerLabels={answerLabels}
+      nextPage="wm10-physical-fitness"
+    />
   );
 }
