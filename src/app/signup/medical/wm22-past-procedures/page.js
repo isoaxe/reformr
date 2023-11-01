0d39c058896b7fb649ba13e33657713cb3def1f9
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-01 22:09:07 +0700

    Build question wm22 with ChooseMultiple
    Use the standardised component so we only need to pass in the data that changes page-to-page.
    This is the final question in the medical quiz.

---
+++ src/app/signup/medical/wm22-past-procedures/page.js
@@ -0,0 +1,26 @@
+import ChooseMultiple from '@/components/quiz/choose-multiple';
+
+export default function PastProcedures() {
+  const heading = [
+    'Last question! Have you ever had any of the following',
+    'weight loss procedures?',
+    '',
+  ];
+  const answerLabels = [
+    'Lab band',
+    'Roux-en-Y gastric bypass',
+    'Sleeve gastrectomy',
+    'Gastric balloon',
+    'Other',
+    'None',
+  ];
+
+  return (
+    <ChooseMultiple
+      heading={heading}
+      questionId="wm22_past_procedures"
+      answerLabels={answerLabels}
+      nextPage="book-telehealth"
+    />
+  );
+}
