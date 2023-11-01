a909b8fffbe174dea65e764e36a39846da983ffc
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-01 09:26:38 +0700

    Refactor wm07 with radio-list component
    Can use RadioList to implement the question for this page in a standardised manner.

--- src/app/signup/medical/wm07-lasting-results/page.js
+++ src/app/signup/medical/wm07-lasting-results/page.js
@@ -1,51 +1,15 @@
-'use client';
+import RadioList from '@/components/quiz/radio-list';
 
-import { useState } from 'react';
-import { Radio, RadioGroup, Typography, FormControlLabel } from '@mui/material';
-import Button from '@/components/quiz/button';
-import { useCookieState } from '@/util/hooks';
-
-/* Multiple choice question with a single answer. */
-export default function FreqLossOfControl() {
-  const [answer, setAnswer] = useState('');
-
-  useCookieState('medical', 'wm07_lasting_results', setAnswer);
-
-  function RadioOption({ label }) {
-    return (
-      <FormControlLabel
-        value={label}
-        control={<Radio />}
-        label={
-          <Typography className="mt-1 text-lg md:text-xl xl:text-2xl">
-            {label}
-          </Typography>
-        }
-      />
-    );
-  }
+export default function LastingResults() {
+  const heading = ['Did you see', 'lasting results?', ''];
+  const answerLabels = ['Yes', 'Sort of', 'Nope'];
 
   return (
-    <main className="mx-auto flex max-w-2xl flex-col">
-      <p className="mb-6">
-        Did you see <span className="font-semibold">lasting results</span>?
-      </p>
-      <RadioGroup
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        sx={{ mb: 3, width: 'fit-content' }}
-      >
-        <RadioOption label="Yes" />
-        <RadioOption label="Sort of" />
-        <RadioOption label="Nope" />
-      </RadioGroup>
-      <Button
-        text="Ok"
-        link="./hope-and-change"
-        state={{ wm07_lasting_results: answer }}
-        isDisabled={!answer}
-        quiz="medical"
-      />
-    </main>
+    <RadioList
+      heading={heading}
+      questionId="wm07_lasting_results"
+      answerLabels={answerLabels}
+      nextPage="hope-and-change"
+    />
   );
 }
