4a017e991678fec602c469ec290b6cfa558a02cf
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-02 11:41:54 +0700

    Improve medical quiz layout for mobile
    Add vertical padding to signup layout so that there's some space between the button and the bottom of the screen. Also adds space between the bottom of the navbar and the top of the heading.

--- src/app/signup/layout.js
+++ src/app/signup/layout.js
@@ -4,7 +4,7 @@ export default function ScreeningLayout({ children }) {
   return (
     <>
       <FocusNavbar />
-      <div className="mt-12 flex min-h-[calc(100vh-3rem)] items-center text-xl md:mt-16 md:min-h-[calc(100vh-4rem)] md:text-2xl xl:text-3xl">
+      <div className="mt-12 flex min-h-[calc(100vh-3rem)] items-center py-6 text-xl md:mt-16 md:min-h-[calc(100vh-4rem)] md:text-2xl xl:text-3xl">
         <div className="mx-auto flex w-full max-w-md px-4 xs:px-9 sm:max-w-6xl">
           {children}
         </div>
