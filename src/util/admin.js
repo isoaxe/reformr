91ae3acc4b00aff058ab16a3a96a878c2ff7086e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-04 09:03:43 +0700

    Setup Firebase admin
    Initialises the Firebase Admin SDK for the backend. This is done separately to the client-side instantiation that handles Firestore and Auth currently.

---
+++ src/util/admin.js
@@ -0,0 +1,31 @@
+/* NOTE: This is for server-side use only. */
+
+import admin from 'firebase-admin';
+
+function formatFirebasePrivateKey(key) {
+  return key.replace(/\\n/g, '\n');
+}
+
+function createFirebaseAdminApp(params) {
+  const privateKey = formatFirebasePrivateKey(params.rawPrivateKey);
+  const { projectId, clientEmail } = params;
+
+  /* If already created, return the same instance */
+  if (admin.apps.length > 0) return admin.app();
+
+  /* Create certificate */
+  const cert = admin.credential.cert({ projectId, clientEmail, privateKey });
+
+  /* Initialize Admin app */
+  return admin.initializeApp({ credential: cert, projectId });
+}
+
+export async function initializeAdmin() {
+  const params = {
+    projectId: 'reformr-health',
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    rawPrivateKey: process.env.FIREBASE_PRIVATE_KEY,
+  };
+
+  return createFirebaseAdminApp(params);
+}
