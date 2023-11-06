5dc7a74e7d462b24383fc599cb73e0fc76ec92ed
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-06 16:21:23 +0700

    Add submit button to payment wrapper
    Add button to submit payment details and handler that executes the logic.

--- src/app/signup/payments/payment-wrapper.js
+++ src/app/signup/payments/payment-wrapper.js
@@ -1,9 +1,31 @@
+import { Button } from '@mui/material';
 import { PaymentElement } from '@stripe/react-stripe-js';
+import { useStripe, useElements } from '@stripe/react-stripe-js';
+import { BASE_URL } from '@/util/urls';
 
 export default function PaymentWrapper() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!stripe || !elements) return;
+    await stripe.confirmPayment({
+      elements,
+      confirmParams: { return_url: `${BASE_URL}/signup/identity-verification` },
+    });
+  }
+
   return (
     <section>
       <PaymentElement />
+      <Button
+        onClick={handleSubmit}
+        variant="outlined"
+        className="mt-4 w-full md:text-lg"
+      >
+        Submit
+      </Button>
     </section>
   );
 }
