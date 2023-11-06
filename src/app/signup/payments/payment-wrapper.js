0f459acaf0e4dd83214206fe4042d4b0cdb6770e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-06 20:51:56 +0700

    Add message and loading states for payments
    Store error messages to state if they occur. Also set and clear a flag to monitor if the API request is in progress or not.

--- src/app/signup/payments/payment-wrapper.js
+++ src/app/signup/payments/payment-wrapper.js
@@ -1,19 +1,27 @@
-import { Button } from '@mui/material';
+import { useState } from 'react';
 import { PaymentElement } from '@stripe/react-stripe-js';
 import { useStripe, useElements } from '@stripe/react-stripe-js';
+import { Button } from '@mui/material';
 import { BASE_URL } from '@/util/urls';
 
 export default function PaymentWrapper() {
+  const [message, setMessage] = useState('');
+  const [isLoading, setLoading] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
 
   async function handleSubmit(event) {
     event.preventDefault();
     if (!stripe || !elements) return;
-    await stripe.confirmPayment({
+    setLoading(true);
+    const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: { return_url: `${BASE_URL}/signup/identity-verification` },
     });
+    if (error.type === 'card_error' || error.type === 'validation_error')
+      setMessage(error.message);
+    else setMessage('An unexpected error occured.');
+    setLoading(false);
   }
 
   return (
