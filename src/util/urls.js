e2235fceefb441aab7500cd93ecc8493fd55c03d
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-06 16:03:40 +0700

    Add BASE_URL to constants
    Determines what the start of the URL should be based on the current URL. Effectively truncates the current URL for the page.

--- src/util/urls.js
+++ src/util/urls.js
@@ -1,2 +1,11 @@
 // Social Media
 export const INSTAGRAM = 'https://www.instagram.com/reformr.nz';
+
+/* Check what URL is currently being used. */
+const currentUrl = window.location.href;
+let baseUrl;
+if (currentUrl.includes('localhost')) baseUrl = 'http://localhost:3000';
+else if (currentUrl.includes('.app')) baseUrl = 'https://reformr.vercel.app';
+else if (currentUrl.includes('.nz')) baseUrl = 'https://reformr.nz';
+else if (currentUrl.includes('.com')) baseUrl = 'https://reformr.com';
+export const BASE_URL = baseUrl;
