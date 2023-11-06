f3bd852aeeac2cd1b4dc10bf1052a78cbd5def66
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-06 13:44:23 +0700

    Implement Stripe Elements on payments page
    Call the payments route using email from cookies. This will return a client secret which is used with appearance props to configure PaymentElement.

--- src/app/signup/payments/page.js
+++ src/app/signup/payments/page.js
@@ -1,12 +1,46 @@
 'use client';
 
+import { useState, useEffect } from 'react';
+import { useCookies } from 'next-client-cookies';
+import { Elements, PaymentElement } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { STRIPE_PUBLIC_KEY } from '@/util/constants';
+
+const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
+
 export default function Payments() {
+  const [options, setOptions] = useState({});
+  const cookies = useCookies();
+
+  useEffect(() => {
+    const email = cookies.get('email');
+    /* Fetch the client secret from the server and use to set options for Elements. */
+    async function getElementsOptions() {
+      const response = await fetch('/api/payments', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+      const subscription = await response.json();
+      const { clientSecret } = subscription;
+      const appearance = { theme: 'stripe', labels: 'floating' };
+      const options = { clientSecret, appearance };
+      setOptions(options);
+    }
+    getElementsOptions();
+  }, [cookies]);
+
   return (
     <main className="min-h-screen">
       <h1 className="mt-16 p-4 text-2xl md:p-8 md:text-4xl">
         This is the<span className="font-bold text-blue-600"> Payments </span>
         page
       </h1>
+      {Object.keys(options).length && (
+        <Elements stripe={stripePromise} options={options}>
+          <PaymentElement />
+        </Elements>
+      )}
     </main>
   );
 }
