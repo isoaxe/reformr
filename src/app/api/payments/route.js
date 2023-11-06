438095e0b3441b751a664faae27bd968948c4be3
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-06 11:12:54 +0700

    Create customer and subscription from payments
    Implement the two Stripe helper methods in the payments route so a new subscription is created when the route is called with the users email.

--- src/app/api/payments/route.js
+++ src/app/api/payments/route.js
@@ -1,9 +1,13 @@
 import { NextResponse } from 'next/server';
+import { createCustomer, createSubscription } from '@/util/stripe';
 
 /* Sign up a new user and create a monthly subscription payment plan. */
 export async function POST(request) {
   const data = await request.json();
-  const { paymentInfo } = data;
+  const { email } = data;
+  const userId = await createCustomer(email);
+  const { subscriptionId, clientSecret, paymentIntentId } =
+    await createSubscription(userId);
 
   return NextResponse.json({ success: true });
 }
