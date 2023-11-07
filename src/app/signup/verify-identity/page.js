3ff6ec1dd9f7f1bcf29846b64bc4cabefe4fa22e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-07 15:10:18 +0700

    Create the verify-identity page
    The usual placeholder until this page gets built. Just so we don't get a 404 in the meantime whilst testing.

---
+++ src/app/signup/verify-identity/page.js
@@ -0,0 +1,11 @@
+export default function VerifyIdentity() {
+  return (
+    <main className="min-h-screen">
+      <h1 className="p-4 text-2xl md:p-8 md:text-4xl">
+        This is the
+        <span className="font-bold text-blue-600"> identity verification </span>
+        page
+      </h1>
+    </main>
+  );
+}
