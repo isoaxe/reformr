9ee7baf5a954d6d1891a669118e8061206b2cfe2
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-07 22:30:34 +0700

    Rearrange declarations in constants.js
    Keep each key and their ternary selection together rather than lumping the ternaries together.

--- src/util/constants.js
+++ src/util/constants.js
@@ -5,17 +5,16 @@ const stripePublicKeyTest =
   'pk_test_51MercyFPKktQy8tpfcK2JfsIQcXMMpvl04HKmGpsYXwEvSziRHaUvxPGKnBu7khVchuIBj2vzLHuaOj9I0xLxVJP00a1f3fm93';
 const stripePublicKeyLive =
   'pk_live_51MercyFPKktQy8tpvFWJwqLJByyrMptoggDwhmEt5yz5Q4omJ5RoZvPyWoukSMx9gSUIV0DwWFAyV2wHnMhKXOID00zz3NEMqc';
-const stripeSecretKeyTest = process.env.STRIPE_SECRET_KEY_TEST;
-const stripeSecretKeyLive = process.env.STRIPE_SECRET_KEY_LIVE;
-
 export const STRIPE_PUBLIC_KEY = isLive
   ? stripePublicKeyLive
   : stripePublicKeyTest;
+
+const stripeSecretKeyTest = process.env.STRIPE_SECRET_KEY_TEST;
+const stripeSecretKeyLive = process.env.STRIPE_SECRET_KEY_LIVE;
 export const STRIPE_SECRET_KEY = isLive
   ? stripeSecretKeyLive
   : stripeSecretKeyTest;
 
 const stripePriceIdTest = 'price_1O9hv3FPKktQy8tphRMSlqgV';
 const stripePriceIdLive = 'price_1NlkQrFPKktQy8tpqjntJXwq';
-
 export const STRIPE_PRICE_ID = isLive ? stripePriceIdLive : stripePriceIdTest;
