14d75387b7e0020542785a946a1f149161c04f0c
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-07 09:37:01 +0700

    Add placeholders to address fields
    To indicate the suggested content type of the field. Wanted to leave the labels more generic since the placeholder text doesn't suit rural users as well.

--- src/app/signup/payments/address.js
+++ src/app/signup/payments/address.js
@@ -5,21 +5,25 @@ export default function Address({ address, setAddress }) {
     <div className="my-8 w-full">
       <AddressInput
         label="Address 1"
+        placeholder="Street name and number"
         text={address?.address1 || ''}
         setText={(input) => setAddress({ ...address, address1: input })}
       />
       <AddressInput
         label="Address 2"
+        placeholder="Suburb"
         text={address?.address2 || ''}
         setText={(input) => setAddress({ ...address, address2: input })}
       />
       <AddressInput
         label="Address 3"
+        placeholder="Town / city"
         text={address?.address3 || ''}
         setText={(input) => setAddress({ ...address, address3: input })}
       />
       <AddressInput
-        label="Zip Code"
+        label="Post Code"
+        placeholder="Post code"
         text={address?.postcode || ''}
         setText={(input) => setAddress({ ...address, postcode: input })}
       />
--- src/components/quiz/address-input.js
+++ src/components/quiz/address-input.js
@@ -1,12 +1,13 @@
 import { TextField } from '@mui/material';
 
-export function AddressInput({ text, setText, label }) {
+export function AddressInput({ text, setText, label, placeholder }) {
   return (
     <TextField
       variant="standard"
       value={text}
       onChange={(e) => setText(e.target.value)}
       label={label}
+      placeholder={placeholder}
       sx={{ width: '100%', mb: 1 }}
       InputProps={{ className: 'text-lg md:text-xl xl:text-2xl' }}
     />
