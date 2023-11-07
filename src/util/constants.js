303843ac6916cad554edc70c68f9f22a29c3b10e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-07 14:13:56 +0700

    Make local constants camelcase
    Make the constants that aren't exported camelCase. Preserve ALL_CAPS for ones that get exported so we don't have them everywhere.

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,17 +1,18 @@
 /* Stripe keys for the SquareSpace account. */
 const isLive = false;
 
-const STRIPE_PUBLIC_KEY_TEST =
+const stripePublicKeyTest =
   'pk_test_51MercyFPKktQy8tpfcK2JfsIQcXMMpvl04HKmGpsYXwEvSziRHaUvxPGKnBu7khVchuIBj2vzLHuaOj9I0xLxVJP00a1f3fm93';
-const STRIPE_SECRET_KEY_TEST = process.env.STRIPE_SECRET_KEY_TEST;
-const STRIPE_PUBLIC_KEY_LIVE =
+const stripePublicKeyLive =
   'pk_live_51MercyFPKktQy8tpvFWJwqLJByyrMptoggDwhmEt5yz5Q4omJ5RoZvPyWoukSMx9gSUIV0DwWFAyV2wHnMhKXOID00zz3NEMqc';
-const STRIPE_SECRET_KEY_LIVE = process.env.STRIPE_SECRET_KEY_LIVE;
+const stripeSecretKeyTest = process.env.STRIPE_SECRET_KEY_TEST;
+const stripeSecretKeyLive = process.env.STRIPE_SECRET_KEY_LIVE;
 
 export const STRIPE_PUBLIC_KEY = isLive
-  ? STRIPE_PUBLIC_KEY_LIVE
-  : STRIPE_PUBLIC_KEY_TEST;
+  ? stripePublicKeyLive
+  : stripePublicKeyTest;
 export const STRIPE_SECRET_KEY = isLive
-  ? STRIPE_SECRET_KEY_LIVE
-  : STRIPE_SECRET_KEY_TEST;
+  ? stripeSecretKeyLive
+  : stripeSecretKeyTest;
+
 export const STRIPE_PRICE_ID = 'price_1O9hv3FPKktQy8tphRMSlqgV';
