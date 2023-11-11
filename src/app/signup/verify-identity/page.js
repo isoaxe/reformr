977114bd0a9cdeadec682fac0de1f1d39bdff3ff
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-11 16:09:17 +0700

    Update identity verification button to use MUI
    Use Material UI rather than the standard HTML button element. Then tweak layout so it's positioned appropriately.

--- src/app/signup/verify-identity/page.js
+++ src/app/signup/verify-identity/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
+import { Button } from '@mui/material';
 import { STRIPE_PUBLIC_KEY } from '@/util/constants';
 
 const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
@@ -28,16 +29,22 @@ function VerifyButton({ stripePromise }) {
   }, [stripePromise]);
 
   return (
-    <button role="link" disabled={!stripe} onClick={handleClick}>
-      Verify
-    </button>
+    <Button
+      role="link"
+      disabled={!stripe}
+      onClick={handleClick}
+      variant="outlined"
+      className="w-fit text-lg md:text-xl"
+    >
+      Verify Identity
+    </Button>
   );
 }
 
 export default function VerifyIdentity() {
   return (
-    <main className="mx-auto min-h-[calc(100vh-7rem)] w-full max-w-xl">
-      <h1 className="mb-4 text-center text-2xl font-semibold text-sky-600 sm:text-3xl">
+    <main className="mx-auto flex min-h-[calc(100vh-7rem)] w-full max-w-xl flex-col items-center">
+      <h1 className="mb-10 text-center text-2xl font-semibold text-sky-600 sm:text-3xl md:mb-20">
         Verify your identity
       </h1>
       <VerifyButton stripePromise={stripePromise} />
