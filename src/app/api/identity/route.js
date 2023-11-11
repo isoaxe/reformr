f40f54a07d2889b8781cecbcd6c1df2fa623dc46
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-11 17:45:52 +0700

    Check user auth before verifying user from server
    Check the the Firebase user ID passed in from the client matches the one in Firestore.
    If it does, pass the client secret back to the client so the identity verification session can commence. If not, return an error.

--- src/app/api/identity/route.js
+++ src/app/api/identity/route.js
@@ -1,5 +1,7 @@
+import admin from 'firebase-admin';
 import Stripe from 'stripe';
 import { NextResponse } from 'next/server';
+import { getDocId } from '@/util/helpers';
 import { STRIPE_SECRET_KEY } from '@/util/constants';
 
 const stripe = new Stripe(STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
@@ -7,9 +9,21 @@ const stripe = new Stripe(STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
 /* Create new identity session for authenticated users. */
 export async function POST(request) {
   const data = await request.json();
-  const { userId } = data;
+  const { email, userId } = data;
 
   try {
+    /* Get userId from Firestore. */
+    const docId = await getDocId(email);
+    const db = admin.firestore();
+    const user = await db.collection('users').doc(docId).get();
+    const userData = user.data();
+    const firestoreUserId = userData.userId;
+
+    /* Check that userId from client is same as Firestore. */
+    if (userId && userId !== firestoreUserId)
+      return NextResponse.json({ success: false, error: 'User auth failed.' });
+
+    /* Create identity verification session. */
     const verificationSession =
       await stripe.identity.verificationSessions.create({
         type: 'document',
@@ -19,7 +33,7 @@ export async function POST(request) {
 
     return NextResponse.json({ success: true, clientSecret });
   } catch (err) {
-    console.error('Error initialising verification session: ', err);
+    console.error('Error processing identity verification: ', err);
     return NextResponse.json({ success: false, error: err });
   }
 }
