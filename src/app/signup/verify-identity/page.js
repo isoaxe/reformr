1b4f0d7fe9b77b558850ea77996fc9d65c61e95a
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-11 15:06:32 +0700

    Start identity verification session
    Trigger the identity verification session modal to appear when the button is clicked.
    Works with a fake user ID for now since auth checks not yet implemented on the backend.

--- src/app/signup/verify-identity/page.js
+++ src/app/signup/verify-identity/page.js
@@ -9,12 +9,26 @@ const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
 function VerifyButton({ stripePromise }) {
   const [stripe, setStripe] = useState(null);
 
+  async function handleClick(event) {
+    event.preventDefault();
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId: '12345' }),
+    };
+    const response = await fetch('/api/identity', options);
+    const { clientSecret } = await response.json();
+    const { error } = await stripe.verifyIdentity(clientSecret);
+    if (error) console.error('Error creating identity session: ', error);
+    else console.log('✅ Identity session created');
+  }
+
   useEffect(() => {
     stripePromise.then(setStripe);
   }, [stripePromise]);
 
   return (
-    <button role="link" disabled={!stripe}>
+    <button role="link" disabled={!stripe} onClick={handleClick}>
       Verify
     </button>
   );
