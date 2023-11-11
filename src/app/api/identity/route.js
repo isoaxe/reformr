4f571d5c62247114e2508a4696e4be388aeecf51
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-11 14:28:37 +0700

    Add route to create new identity session
    Allow users to create a new identity session. Will add authentication checks later based on Firebase user ID.

---
+++ src/app/api/identity/route.js
@@ -0,0 +1,25 @@
+import Stripe from 'stripe';
+import { NextResponse } from 'next/server';
+import { STRIPE_SECRET_KEY } from '@/util/constants';
+
+const stripe = new Stripe(STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
+
+/* Create new identity session for authenticated users. */
+export async function POST(request) {
+  const data = await request.json();
+  const { userId } = data;
+
+  try {
+    const verificationSession =
+      await stripe.identity.verificationSessions.create({
+        type: 'document',
+        metadata: { user_id: userId },
+      });
+    const clientSecret = verificationSession.client_secret;
+
+    return NextResponse.json({ success: true, clientSecret });
+  } catch (err) {
+    console.error('Error initialising verification session: ', err);
+    return NextResponse.json({ success: false, error: err });
+  }
+}
