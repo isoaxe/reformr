d4a6c45f37758d6102bb12989d35930593fe25f7
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-11 14:13:42 +0700

    Add the identity verification button
    Add the button per the Stripe documentation.

--- src/app/signup/verify-identity/page.js
+++ src/app/signup/verify-identity/page.js
@@ -1,3 +1,25 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { STRIPE_PUBLIC_KEY } from '@/util/constants';
+
+const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
+
+function VerifyButton({ stripePromise }) {
+  const [stripe, setStripe] = useState(null);
+
+  useEffect(() => {
+    stripePromise.then(setStripe);
+  }, [stripePromise]);
+
+  return (
+    <button role="link" disabled={!stripe}>
+      Verify
+    </button>
+  );
+}
+
 export default function VerifyIdentity() {
   return (
     <main className="min-h-[calc(100vh-7rem)]">
@@ -6,6 +28,7 @@ export default function VerifyIdentity() {
         <span className="font-bold text-blue-600"> identity verification </span>
         page
       </h1>
+      <VerifyButton stripePromise={stripePromise} />
     </main>
   );
 }
