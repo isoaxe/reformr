65df37e8bc344ddb674da236f83118030986c915
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-13 22:12:42 +0700

    Create patient dashboard page
    Just a heading and positioning the layout appropriately. Spacings and sizings same as other parts of the site.

---
+++ src/app/main/dashboard/patient/page.js
@@ -0,0 +1,7 @@
+export default function PatientDashboard() {
+  return (
+    <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-row px-4 xs:px-9">
+      <h1 className="p-4 text-2xl md:p-8 md:text-4xl">Patient Details</h1>
+    </main>
+  );
+}
