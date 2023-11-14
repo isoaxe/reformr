2752e1060be11b5d2663c404aca2ca913213a3cd
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-14 09:21:30 +0700

    Tweak positioning of patient dashboard heading
    Effectively removes left padding so that the heading aligns with the items below.

--- src/app/main/dashboard/patient/page.js
+++ src/app/main/dashboard/patient/page.js
@@ -3,7 +3,7 @@ import DropdownItem from '../dropdown-item';
 export default function PatientDashboard() {
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9">
-      <h1 className="p-4 text-2xl text-sky-600 md:p-8 md:text-4xl">
+      <h1 className="py-4 text-2xl text-sky-600 md:py-8 md:text-4xl">
         Patient Details
       </h1>
       <DropdownItem />
