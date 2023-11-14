1df9491fc10b4b551a771261eaea566715c4479d
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-14 15:27:00 +0700

    Get user details from auth in patient dashboard
    Allows us to populate the patient info section with name, email and phone.
    Details taken from the Firebase authentication user object rather than Firestore.

--- src/app/main/dashboard/patient/page.js
+++ src/app/main/dashboard/patient/page.js
@@ -1,26 +1,42 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import { BsFillPersonFill } from 'react-icons/bs';
 import DropdownItem from '../dropdown-item';
-
-/* Props for patient info section. */
-const patientInfoIcon = <BsFillPersonFill size={30} />;
-const patientInfoContent = (
-  <>
-    <div className="flex flex-row">
-      <p className="w-20 font-medium">Name:</p>
-      <p>Your name</p>
-    </div>
-    <div className="my-1 flex flex-row">
-      <p className="w-20 font-medium">Email:</p>
-      <p>Your email</p>
-    </div>
-    <div className="flex flex-row">
-      <p className="w-20 font-medium">Phone:</p>
-      <p>Your phone</p>
-    </div>
-  </>
-);
+import { useAuth } from '@/util/hooks';
 
 export default function PatientDashboard() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const { user } = useAuth();
+
+  /* Props for patient info section. */
+  const patientInfoIcon = <BsFillPersonFill size={30} />;
+  const patientInfoContent = (
+    <>
+      <div className="flex flex-row">
+        <p className="w-20 font-medium">Name:</p>
+        <p>{name}</p>
+      </div>
+      <div className="my-1 flex flex-row">
+        <p className="w-20 font-medium">Email:</p>
+        <p>{email}</p>
+      </div>
+      <div className="flex flex-row">
+        <p className="w-20 font-medium">Phone:</p>
+        <p>{phone}</p>
+      </div>
+    </>
+  );
+
+  useEffect(() => {
+    if (!user) return;
+    setName(user.displayName);
+    setEmail(user.email);
+    setPhone(user.phoneNumber);
+  }, [user]);
+
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9">
       <h1 className="py-4 text-2xl text-sky-600 md:py-8 md:text-4xl">
