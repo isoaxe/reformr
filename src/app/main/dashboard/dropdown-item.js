6934ad6c63053aa04945a5f859e8a6d082cada66
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-14 09:14:47 +0700

    Add collapsible section to patient info item
    Place a divider between the visible and hidden content. Add a placeholder element so there is something to render.
    Also move some styles around so the border wraps the expanded area too.

--- src/app/main/dashboard/dropdown-item.js
+++ src/app/main/dashboard/dropdown-item.js
@@ -3,15 +3,16 @@
 import { useState } from 'react';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { Spiral as Hamburger } from 'hamburger-react';
+import Collapse from '@mui/material/Collapse';
 
 export default function DropdownItem() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="text-lg md:text-xl lg:text-2xl">
+    <section className="rounded border border-slate-400 px-4 py-4 text-lg md:text-xl lg:text-2xl">
       <div
         onClick={() => setOpen(!isOpen)}
-        className="flex w-full flex-row items-center rounded border border-slate-400 px-4 py-4 hover:cursor-pointer"
+        className="flex w-full flex-row items-center hover:cursor-pointer"
       >
         <BsFillPersonFill size={30} />
         <div className="flex w-full flex-row items-center justify-between">
@@ -24,6 +25,11 @@ export default function DropdownItem() {
           />
         </div>
       </div>
+      <Collapse in={isOpen}>
+        {/* This part is initially hidden. */}
+        <div className="mb-4 w-full border border-b-slate-400 pt-1"></div>
+        <p>Some placeholder content for now</p>
+      </Collapse>
     </section>
   );
 }
