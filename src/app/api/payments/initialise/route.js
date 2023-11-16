10c881472ad009fd41f3dc1fd1fe231d3a5e7fe3
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-16 11:50:42 +0700

    Add isActive flag to subscription on init
    A boolean value to signify if the subscription is active or not. Gets added to user data in Firestore when a subscription gets created.

--- src/app/api/payments/initialise/route.js
+++ src/app/api/payments/initialise/route.js
@@ -33,6 +33,7 @@ export async function POST(request) {
     /* Create a new subscription if not in Firestore, */
     const stripeUid = await createCustomer(name, email);
     const subscription = await createSubscription(stripeUid);
+    subscription.isActive = true;
 
     /* Save Stripe payments data to Firestore if not already there. */
     const paymentData = {
