22ba6684312b97ce0a3710ba5b201649e4060d37
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-16 23:11:38 +0700

    Center heading for patient dashboard
    In line with the payments and identity pages. More importantly, fits in better with the current dashboard content.

--- src/app/main/dashboard/patient/page.js
+++ src/app/main/dashboard/patient/page.js
@@ -159,7 +159,7 @@ export default function PatientDashboard() {
 
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9">
-      <h1 className="py-4 text-2xl font-semibold text-sky-600 md:py-8 md:text-4xl">
+      <h1 className="py-4 text-center text-2xl font-semibold text-sky-600 md:py-8 md:text-4xl">
         Patient Details
       </h1>
       <DropdownItem
