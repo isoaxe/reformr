6a3878bb8f642e2bb9f9a19f440574980676c630
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-17 21:11:45 +0700

    Add a server route for pausing a subscription
    Calls the helper function to pause with Stripe first. Then increments number of boxes skipped on Firestore and sets the isPaid flag to false there.

---
+++ src/app/api/payments/pause-sub/route.js
@@ -0,0 +1,35 @@
+import admin from 'firebase-admin';
+import { NextResponse } from 'next/server';
+import { pauseSubscription } from '@/util/stripe';
+import { getDocId } from '@/util/helpers';
+import { initialiseAdmin } from '@/util/admin';
+
+/* Pause Stripe subscription and update Firestore data. */
+export async function POST(request) {
+  const data = await request.json();
+  const { subId, email } = data;
+
+  try {
+    /* Pause subscription with Stripe for one month. */
+    await pauseSubscription(subId);
+
+    /* Increment numBoxesSkipped and turn off isPaid flag in Firestore. */
+    const docId = await getDocId(email);
+    await initialiseAdmin();
+    const db = admin.firestore();
+    const userRef = db.collection('users').doc(docId);
+    const userDoc = await userRef.get();
+    const userData = userDoc.data();
+    let { numBoxesSkipped } = userData.payments;
+    numBoxesSkipped++;
+    await userRef.set(
+      { payments: { numBoxesSkipped, isPaid: false } },
+      { merge: true }
+    );
+
+    return NextResponse.json({ success: true });
+  } catch (err) {
+    console.error('Error pausing subscription: ', err);
+    return NextResponse.json({ success: false, error: err });
+  }
+}
