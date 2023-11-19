b72c5c67bbb54bb83d2e7ee117441999a578da97
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-19 14:09:51 +0700

    Create basic admin dashboard
    Just a heading for now. Positioning margins, padding and dimensions are as with patient dashboard.

---
+++ src/app/main/dashboard/admin/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+export default function AdminDashboard() {
+  return (
+    <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9">
+      <h1 className="py-4 text-center text-2xl font-semibold text-sky-600 md:py-8 md:text-4xl">
+        Create a new user
+      </h1>
+    </main>
+  );
+}
