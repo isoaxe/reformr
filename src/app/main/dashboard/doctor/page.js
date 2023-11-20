301bc931c6135f6920b0866a9d55f77855c4b505
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-20 21:20:49 +0700

    Add flag for page load on doctor dashboard
    We can assume that the page has loaded after one second. Store that status to state.

--- src/app/main/dashboard/doctor/page.js
+++ src/app/main/dashboard/doctor/page.js
@@ -1,4 +1,16 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
 export default function DoctorDashboard() {
+  const [isPageLoaded, setPageLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoaded(true); // assume user fetched within a second.
+    }, 1000);
+  }, []);
+
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9"></main>
   );
