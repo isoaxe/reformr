8ddf494536ac7439f3cd06e65d2e30cac28b3dfb
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-20 21:48:03 +0700

    Check user role on doctor dashboard
    Get the role from the auth object on user load and save to state.

--- src/app/main/dashboard/doctor/page.js
+++ src/app/main/dashboard/doctor/page.js
@@ -1,15 +1,28 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { auth } from '@/util/firebase';
+import { useAuth } from '@/util/hooks';
 
 export default function DoctorDashboard() {
   const [isPageLoaded, setPageLoaded] = useState(false);
+  const [role, setRole] = useState('');
+  const { user } = useAuth();
 
   useEffect(() => {
     setTimeout(() => {
       setPageLoaded(true); // assume user fetched within a second.
     }, 1000);
   }, []);
+
+  useEffect(() => {
+    if (!user) return;
+    const getRole = async () =>
+      await auth.currentUser
+        .getIdTokenResult()
+        .then((res) => setRole(res.claims.role));
+    getRole();
+  }, [user]);
 
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9"></main>
