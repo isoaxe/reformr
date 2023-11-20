b5d7501f884a200810fac9f91426f6c5eecd4b8a
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-20 21:58:19 +0700

    Redirect from doctor dashboard based on role
    Don't bother with timeout to determine if page is loaded for this dashboard. Instead just check the role as this is the determining factor for a redirect.

--- src/app/main/dashboard/doctor/page.js
+++ src/app/main/dashboard/doctor/page.js
@@ -6,16 +6,9 @@ import { auth } from '@/util/firebase';
 import { useAuth } from '@/util/hooks';
 
 export default function DoctorDashboard() {
-  const [isPageLoaded, setPageLoaded] = useState(false);
   const [role, setRole] = useState('');
   const router = useRouter();
   const { user } = useAuth();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setPageLoaded(true); // assume user fetched within a second.
-    }, 1000);
-  }, []);
 
   useEffect(() => {
     if (!user) return;
@@ -27,9 +20,9 @@ export default function DoctorDashboard() {
   }, [user]);
 
   useEffect(() => {
-    if (!isPageLoaded) return;
+    if (role === '') return;
     if (role !== 'doctor') router.push('/main/login'); // redirect to login if not doctor.
-  }, [isPageLoaded, role, router]);
+  }, [role, router]);
 
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-full max-w-3xl flex-col px-4 xs:px-9"></main>
