1d4173fa380cd98c019b2cd0757284a7b0305d8e
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-21 23:22:30 +0700

    Add logout button to doctor dashboard
    Forgot to add this feature to the previous PR.

--- src/app/main/dashboard/doctor/page.js
+++ src/app/main/dashboard/doctor/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { FormControl, Select, MenuItem } from '@mui/material';
+import { FormControl, Select, MenuItem, Button } from '@mui/material';
 import { auth } from '@/util/firebase';
 import { useAuth } from '@/util/hooks';
 
@@ -11,7 +11,7 @@ export default function DoctorDashboard() {
   const [patients, setPatients] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const router = useRouter();
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
 
   const statusOptions = [
     'pending',
@@ -107,6 +107,13 @@ export default function DoctorDashboard() {
       {patients?.map((patient, idx) => (
         <Patient patient={patient} key={idx} />
       ))}
+      <Button
+        variant="outlined"
+        className="mx-auto mt-5 w-full max-w-xs text-lg md:text-xl"
+        onClick={logout}
+      >
+        Logout
+      </Button>
     </main>
   );
 }
