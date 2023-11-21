7ea280da0380074701182d9b5a0803fcccd32fd8
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-21 22:56:14 +0700

    Create pharmacist dashboard page
    Displays table title and column headings similar to the doctor dashboard.

---
+++ src/app/main/dashboard/pharmacist/page.js
@@ -0,0 +1,14 @@
+export default function PharmacistDashboard() {
+  return (
+    <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-fit max-w-3xl flex-col px-4 xs:px-9">
+      <h1 className="mb-2 pt-4 text-center text-xl font-semibold text-sky-600 md:pt-8 md:text-2xl">
+        Current Patients
+      </h1>
+      <div className="flex flex-row font-semibold">
+        <p className="w-40">Name</p>
+        <p className="w-64">Email</p>
+        <p>Order Status</p>
+      </div>
+    </main>
+  );
+}
