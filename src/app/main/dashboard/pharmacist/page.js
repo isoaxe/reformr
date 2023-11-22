a86ef7bf9af7939b792b7c3f2d343865b91b4d67
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-22 08:53:14 +0700

    Add isPageLoaded flag for pharm dashboard
    Sets to true after one second. As with other dashboards, it's used as a crude indicator that all initial state on the page has loaded (e.g. user from useAuth hook or role from auth etc).

--- src/app/main/dashboard/pharmacist/page.js
+++ src/app/main/dashboard/pharmacist/page.js
@@ -1,4 +1,16 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
 export default function PharmacistDashboard() {
+  const [isPageLoaded, setPageLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoaded(true); // assume user fetched within a second.
+    }, 1000);
+  }, []);
+
   return (
     <main className="mx-auto flex min-h-[calc(100vh-23rem)] w-fit max-w-3xl flex-col px-4 xs:px-9">
       <h1 className="mb-2 pt-4 text-center text-xl font-semibold text-sky-600 md:pt-8 md:text-2xl">
