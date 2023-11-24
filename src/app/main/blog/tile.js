452f721e545c669231dfbc0fd6fe5d0d31996fb6
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-24 14:44:38 +0700

    Allow blog tiles to wrap correctly
    Needed to restructure the tile in order to get spacing between tiles and 4 per row.

--- src/app/main/blog/tile.js
+++ src/app/main/blog/tile.js
@@ -10,21 +10,23 @@ export default function Tile(props) {
   });
 
   return (
-    <div className="group relative m-2 flex aspect-video w-1/4 flex-col transition hover:scale-[.98] hover:cursor-pointer">
-      <div className="absolute h-full w-full overflow-hidden rounded">
-        <Image
-          src={image}
-          alt="A thumbnail image relating to the blog article."
-          fill
-          className="z-0 object-cover transition-all group-hover:scale-[1.15]"
-          sizes="100vw"
-        />
-        <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-zinc-600 md:border-none"></div>
-      </div>
-      <div className="relative my-auto p-6 font-medium">
-        <p>{formattedDate}</p>
-        <h1 className="my-2 text-2xl">{title}</h1>
-        <h2 className="text-xl">{description}</h2>
+    <div className="w-1/4">
+      <div className="group relative flex w-full flex-col p-2 transition hover:scale-[.98] hover:cursor-pointer">
+        <div className="relative aspect-video w-full overflow-hidden rounded">
+          <Image
+            src={image}
+            alt="A thumbnail image relating to the blog article."
+            fill
+            className="z-0 object-cover transition-all group-hover:scale-[1.15]"
+            sizes="100vw"
+          />
+          <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-zinc-600 md:border-none"></div>
+        </div>
+        <div className="absolute my-auto p-6 font-medium">
+          <p>{formattedDate}</p>
+          <h1 className="my-2 text-2xl">{title}</h1>
+          <h2 className="text-xl">{description}</h2>
+        </div>
       </div>
     </div>
   );
