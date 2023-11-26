e088bd765b4ede9ad97a4ac05ba1a3d40130ede7
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-26 11:14:56 +0700

    Add sections 3-5 of second article
    Similar in style to one another. Just headings and paragraphs.

--- src/app/main/blog/(articles)/tips-for-injecting/page.js
+++ src/app/main/blog/(articles)/tips-for-injecting/page.js
@@ -33,6 +33,46 @@ export default function TipsForInjecting() {
         </a>
         .
       </p>
+      <h2 className="mt-16 text-4xl font-medium md:text-5xl">
+        Where should I inject Saxenda?
+      </h2>
+      <p className="mt-8">
+        <a href="https://www.saxenda.com" className="text-sky-700 underline">
+          Saxenda
+        </a>{' '}
+        injections can be administered in the upper arm, abdomen, or front of
+        the thigh. Most users find that injecting into the abdomen is more
+        comfortable. The optimal spot on your stomach is below your belly button
+        and above your hipline. Unlike other injectable medications, there is no
+        need to rotate injection sites with Saxenda, although you may want to
+        switch up locations for your own comfort. If you discover a site that is
+        more suitable for you, feel free to continue using it.
+      </p>
+      <h2 className="mt-16 text-4xl font-medium md:text-5xl">
+        How should I position myself?
+      </h2>
+      <p className="my-8">
+        Feelings of nervousness, particularly when it comes to injections, can
+        often cause lightheadedness or nausea, as our bodies attempt to warn us
+        of potential danger. Reclining and putting your body into a relaxed
+        position during the administration process of Saxenda can help alleviate
+        some of these nervous feelings and sensations.
+      </p>
+      <p>
+        Ensure that everything you need is within reach, and that you have a
+        clear view of the injection site, typically the abdomen. After
+        administering the dose, continue reclining for a short while to allow
+        yourself time to calm down before congratulating yourself for
+        successfully overcoming a daunting task.
+      </p>
+      <h2 className="mt-16 text-4xl font-medium md:text-5xl">
+        How do I administer Saxenda?
+      </h2>
+      <p className="my-8">
+        Once you have selected your injection site and have reclined yourself
+        into a relaxed position, you are ready to administer your dose of
+        Saxenda. Always wash your hands before administering your dose.
+      </p>
     </main>
   );
 }
