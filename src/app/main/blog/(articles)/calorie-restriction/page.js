d8262455369a2ea4d7b370de3a21336fce0649cc
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-28 10:44:17 +0700

    Complete the twelfth blog article
    Finished the remainder of the twelfth-latest blog article. This was the second article published.

--- src/app/main/blog/(articles)/calorie-restriction/page.js
+++ src/app/main/blog/(articles)/calorie-restriction/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Button from '@/components/button';
 import { blogTileData } from '@/util/data';
 
 export default function IncidentalExercise() {
@@ -29,6 +30,88 @@ export default function IncidentalExercise() {
             sizes="(max-width: 767px) 100vw, 50vw"
           />
         </div>
+      </div>
+      <h2 className="mt-16 text-3xl font-medium xs:text-4xl md:text-5xl">
+        Your body has adapted to the reduced calorie intake
+      </h2>
+      <p className="mt-8">
+        When you start restricting calories, your body goes into survival mode
+        and slows down your metabolism to conserve energy. This adaptation can
+        make it harder to lose weight over time, as your body becomes more
+        efficient at using the limited calories you are consuming. To break
+        through this plateau, you may need to change up your calorie intake or
+        switch to a different weight loss method altogether.
+      </p>
+      <h2 className="mt-16 text-3xl font-medium xs:text-4xl md:text-5xl">
+        You’re not eating enough protein
+      </h2>
+      <p className="mt-8">
+        Protein is essential for maintaining muscle mass and keeping your
+        metabolism functioning properly. If you’re not consuming enough protein
+        while restricting calories, your body may start to break down muscle
+        tissue for energy. This can lead to a slower metabolism and reduced
+        weight loss. Aim to include protein-rich foods in every meal and snack
+        to help maintain muscle mass and keep your metabolism firing.
+      </p>
+      <h2 className="mt-16 text-3xl font-medium xs:text-4xl md:text-5xl">
+        You’re not getting enough sleep
+      </h2>
+      <p className="mt-8">
+        Lack of sleep can interfere with weight loss by disrupting hormones that
+        regulate appetite and metabolism. When you don’t get enough sleep, your
+        body produces more of the hunger hormone ghrelin and less of the satiety
+        hormone leptin, which can lead to overeating and weight gain. Aim for at
+        least seven to eight hours of sleep per night to help support your
+        weight loss efforts.
+      </p>
+      <h2 className="mt-16 text-3xl font-medium xs:text-4xl md:text-5xl">
+        You’re not tracking your calorie intake accurately
+      </h2>
+      <p className="mt-8">
+        It’s easy to underestimate the number of calories you’re consuming,
+        especially when eating out or snacking on packaged foods. Keep track of
+        what you eat and how many calories you’re consuming to ensure you’re
+        staying within your daily calorie budget. Use a food diary or a
+        calorie-tracking app to help you stay on track.
+      </p>
+      <h2 className="mt-16 text-3xl font-medium xs:text-4xl md:text-5xl">
+        You’re not getting enough physical activity
+      </h2>
+      <p className="mb-16 mt-8">
+        While calorie restriction is an important part of weight loss, it’s not
+        the only factor. Incorporating regular physical activity into your
+        routine can help boost your metabolism, burn calories, and support
+        weight loss. Aim for at least 30 minutes of moderate-intensity exercise
+        most days of the week to help break through your weight loss plateau.
+      </p>
+      <p>
+        If you’ve hit a plateau in your weight loss journey despite calorie
+        restriction, its important to remember that there are many factors that
+        can affect weight loss. By addressing these common reasons for a
+        plateau, such as a slow metabolism, insufficient protein intake, lack of
+        sleep, inaccurate tracking of calorie intake, and inadequate physical
+        activity, you can break through your weight loss plateau and achieve
+        your goals. Remember to always prioritize a healthy and sustainable
+        approach to weight loss, and seek professional guidance if necessary.
+      </p>
+      <p className="mt-8">
+        If you’re interested to find out if{' '}
+        <a href="https://reformr.co.nz" className="text-sky-700">
+          Reformr
+        </a>
+        ’s metabolic reset programme might help you break through you’re
+        plateau,{' '}
+        <a href="/signup/screening" className="text-sky-700">
+          take our eligibility quiz
+        </a>
+        .
+      </p>
+      <div className="my-12 text-center">
+        <Button
+          text="Find out if you're eligible"
+          link="/signup/screening"
+          haloShade="dark"
+        />
       </div>
     </main>
   );
