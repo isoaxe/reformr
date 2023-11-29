9e23e575a59844c5649c3fed1c140791809264dd
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-29 13:45:05 +0700

    Add auto-focus to address-input
    Wanted to make sure this component behaved as expected when adding the autoFocus property. There are four of these components on the same page, so it's the top-most one that gets focused as desired.

--- src/components/quiz/address-input.js
+++ src/components/quiz/address-input.js
@@ -3,6 +3,7 @@ import { TextField } from '@mui/material';
 export function AddressInput({ text, setText, label, placeholder }) {
   return (
     <TextField
+      autoFocus
       variant="standard"
       value={text}
       onChange={(e) => setText(e.target.value)}
