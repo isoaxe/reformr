8e6cb05f253b0b4d81e5f6252d0491659c1c3174
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-11-29 09:51:13 +0700

    Add useKeyPress hook
    We need to use this logic on other components than the Button (which is used on most quiz pages, but not all of them).

--- src/util/hooks.js
+++ src/util/hooks.js
@@ -82,3 +82,18 @@ export function useAuthProvider() {
     logout,
   };
 }
+
+/* Runs callback function when Enter key is pressed. */
+export function useKeyPress(callback) {
+  useEffect(() => {
+    const handleKey = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        callback();
+      }
+    };
+
+    document.addEventListener('keydown', handleKey);
+    return () => document.removeEventListener('keydown', handleKey);
+  }, [callback]);
+}
