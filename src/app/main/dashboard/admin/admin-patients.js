bb3a73cfdddd3e731bd70a450c88ee0bfbfdda10
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-12-04 12:13:38 +0700

    Handle when identity not failed on admin dashboard
    Display some text when there are no patients who have failed identity verification.
    This is better than displaying an empty table.

--- src/app/main/dashboard/admin/admin-patients.js
+++ src/app/main/dashboard/admin/admin-patients.js
@@ -41,11 +41,17 @@ export default function AdminPatients({ user }) {
       <h1 className="mb-2 pt-4 text-center text-xl font-semibold text-sky-600 md:pt-8 md:text-2xl">
         Patients with Failed Identity Verification
       </h1>
-      <div className="flex flex-row font-semibold">
-        <p className="w-40">Name</p>
-        <p className="w-64">Email</p>
-        <p className="w-40">Last Payment</p>
-      </div>
+      {!patients.length && isFetched ? (
+        <p className="mt-10 text-center text-slate-700">
+          No patients with failed identity verification.
+        </p>
+      ) : (
+        <div className="flex flex-row font-semibold">
+          <p className="w-40">Name</p>
+          <p className="w-64">Email</p>
+          <p className="w-40">Last Payment</p>
+        </div>
+      )}
       {isFetched ? (
         patients?.map((patient, idx) => <Patient patient={patient} key={idx} />)
       ) : (
