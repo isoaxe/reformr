7f50c13bd9d796cd443d6d34d9f89023a1de32ad
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-12-05 10:56:01 +0700

    Refactor screening in patient data
    Access the screening data via variables declared at the top rather than dot notation as they will be accessed again.
    Save age to state as that requires a calculation.

--- src/app/main/dashboard/doctor/patient-record.js
+++ src/app/main/dashboard/doctor/patient-record.js
@@ -7,6 +7,9 @@ import { Modal } from '@mui/material';
 export default function PatientRecord({ open, setOpen, fireDocId }) {
   const [screening, setScreening] = useState({});
   const [medical, setMedical] = useState({});
+  const [age, setAge] = useState(0);
+  const { firstName, lastName, email, phone, dob, sex, height, weight, bmi } =
+    screening;
 
   useEffect(() => {
     async function getPatientRecord() {
@@ -21,50 +24,56 @@ export default function PatientRecord({ open, setOpen, fireDocId }) {
     if (fireDocId) getPatientRecord();
   }, [fireDocId]);
 
+  useEffect(() => {
+    if (dob) {
+      const age = Math.floor(
+        (new Date() - new Date(dob)) / 31557600000 // 1 year in ms.
+      );
+      setAge(age);
+    }
+  }, [dob]);
+
   return (
     <Modal open={open} onClose={() => setOpen(false)}>
       <section className="mx-auto mt-12 h-[90vh] w-full max-w-5xl rounded bg-slate-300 px-9 md:text-lg">
         <h1 className="mb-2 pt-4 text-center text-2xl font-semibold text-sky-600 md:text-3xl">
           Patient Record
         </h1>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row">
           <div className="flex flex-col">
             <p>
               <span className="font-semibold">Name: </span>
-              {`${screening.firstName} ${screening.lastName}`}
+              {`${firstName} ${lastName}`}
             </p>
             <p>
               <span className="font-semibold">Phone: </span>
-              {screening.phone}
+              {phone}
             </p>
             <p>
               <span className="font-semibold">DoB: </span>
-              {screening.dob}
+              {dob}
             </p>
             <p>
               <span className="font-semibold">Email: </span>
-              {screening.email}
+              {email}
             </p>
           </div>
-          <div className="flex flex-col">
+          <div className="mx-10 flex flex-col">
             <p>
               <span className="font-semibold">Age: </span>
-              {Math.floor(
-                (new Date() - new Date(screening.dob)) / 31557600000 // 1 year in ms.
-              )}{' '}
-              years
+              {age} years
             </p>
             <p>
               <span className="font-semibold">Sex: </span>
-              {screening.sex}
+              {sex}
             </p>
             <p>
               <span className="font-semibold">Height: </span>
-              {`${screening.height}cm`}
+              {`${height}cm`}
             </p>
             <p>
               <span className="font-semibold">Weight: </span>
-              {`${screening.weight}kg`}
+              {`${weight}kg`}
             </p>
           </div>
         </div>
