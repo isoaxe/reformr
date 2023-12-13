2fa138e9c4219f8f7d8097dbc9b1f8e574f76ad1
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-12-13 13:58:46 +0700

    Restyle button for main site
    Make the generic button look more like the one on Squarespace. Do the following:
    + Remove border.
    + Remove text colour change on hover.
    + Change button colour to cyan.
    + Made button corners less rounded.
    + Made button slightly transparent on hover.

--- src/components/button.js
+++ src/components/button.js
@@ -3,9 +3,7 @@ import Link from 'next/link';
 export default function Button({ text, link }) {
   return (
     <Link href={link}>
-      <button
-        className={`rounded-lg border-2 border-violet-900 bg-violet-600 px-4 py-2 text-xl font-medium text-white transition hover:bg-violet-700 hover:text-teal-100 md:px-6 md:py-3 md:text-2xl`}
-      >
+      <button className="rounded bg-cyan-400 px-4 py-2 text-xl font-medium text-white transition hover:bg-cyan-500 hover:bg-opacity-80 md:px-6 md:py-3 md:text-2xl">
         {text}
       </button>
     </Link>
