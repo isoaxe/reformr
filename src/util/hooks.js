ed965326b23110560efb19f6c0390eaccd00c2d3
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-12-13 22:13:44 +0700

    Add useDeviceType hook
    Determines the type of device that the user is accessing the site from. Can be 'Mobile', 'Tablet' or 'Desktop'.

--- src/util/hooks.js
+++ src/util/hooks.js
@@ -129,3 +129,29 @@ export function useScrollPosition() {
 
   return scrollPosition;
 }
+
+/* Determines device type. */
+export function useDeviceType() {
+  const [device, setDevice] = useState('');
+
+  useEffect(() => {
+    function handleDeviceDetection() {
+      const userAgent = navigator.userAgent.toLowerCase();
+      const isMobile =
+        /iphone|ipad|ipod|android|blackberry|windows phone/g.test(userAgent);
+      const isTablet =
+        /(ipad|tablet|playbook|silk)|(android(?!.*mobile))/g.test(userAgent);
+
+      if (isMobile) setDevice('Mobile');
+      else if (isTablet) setDevice('Tablet');
+      else setDevice('Desktop');
+    }
+
+    handleDeviceDetection();
+    window.addEventListener('resize', handleDeviceDetection);
+
+    return () => window.removeEventListener('resize', handleDeviceDetection);
+  }, []);
+
+  return device;
+}
