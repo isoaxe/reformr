3879ed46c06c8696f2b6a2bdb8cc2c854a803f29
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-12-14 16:29:37 +0700

    Add custom cursor to type animation
    Remove the default cursor and add a custom one that is much thinner and pulses like the Squarespace one.

--- src/app/main/home/hero.js
+++ src/app/main/home/hero.js
@@ -9,7 +9,7 @@ import { useDeviceType } from '@/util/hooks';
 
 /* Hero section that the user first sees at the top of the homepage. */
 export default function Hero() {
-  const interval = 2000;
+  const interval = 3000;
   const deviceType = useDeviceType();
 
   return (
@@ -21,6 +21,7 @@ export default function Hero() {
       />
       <div className="relative flex h-full w-full flex-col justify-center bg-blue-600 bg-opacity-20 px-4 text-white xs:pl-12 lg:pl-32">
         <TypeAnimation
+          cursor={false}
           sequence={[
             'TRANSFORMATIVE',
             interval,
@@ -39,8 +40,8 @@ export default function Hero() {
             'REVOLUTIONARY',
             interval,
           ]}
-          speed={50}
-          className="text-4xl font-extrabold text-white xs:text-5xl md:text-6xl lg:text-7xl"
+          speed={70}
+          className="typing-cursor text-4xl font-extrabold text-white xs:text-5xl md:text-6xl lg:text-7xl"
           repeat={Infinity}
         />
         <h1 className="mt-4 text-4xl font-extrabold xs:text-5xl md:text-6xl lg:text-7xl">
