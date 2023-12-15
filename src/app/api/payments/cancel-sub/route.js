531db9fb545fc85efbd20321517bfc9c87383ea4
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-12-15 16:46:50 +0700

    Add auth token for /api/payments/cancel-sub
    Get auth token from currently signed in user. Pass to backend and authenticate. Do not save data if user is not the same as that whose data is being modified.

--- src/app/api/payments/cancel-sub/route.js
+++ src/app/api/payments/cancel-sub/route.js
@@ -1,4 +1,5 @@
 import admin from 'firebase-admin';
+import { getAuth } from 'firebase-admin/auth';
 import { NextResponse } from 'next/server';
 import { cancelSubscription } from '@/util/stripe';
 import { getDocId } from '@/util/helpers';
@@ -7,17 +8,31 @@ import { initialiseAdmin } from '@/util/admin';
 /* Cancel Stripe subscription and set Firestore flag. */
 export async function POST(request) {
   const data = await request.json();
-  const { subId, email } = data;
+  const { subId, email, fireToken } = data;
 
   try {
+    /* Get current user ID. */
+    await initialiseAdmin();
+    const user = await getAuth().verifyIdToken(fireToken);
+    const { uid } = user;
+
+    /* Get user ID from Firestore. */
+    const docId = await getDocId(email);
+    const db = admin.firestore();
+    const patientRef = db.collection('patients').doc(docId);
+    const patientDoc = await patientRef.get();
+    const { userId } = patientDoc.data();
+
+    /* Check that user is modifying their own data. */
+    if (userId !== uid) {
+      const error = 'Not authorised to modify this data.';
+      return NextResponse.json({ error });
+    }
+
     /* Cancel subscription with Stripe at end of billing period. */
     await cancelSubscription(subId);
 
-    /* Set isCancelled flag for subscription to true in Firestore. */
-    const docId = await getDocId(email);
-    await initialiseAdmin();
-    const db = admin.firestore();
-    const patientRef = db.collection('patients').doc(docId);
+    /* Set subscription to cancelled in Firestore. */
     await patientRef.set(
       { payments: { subscription: { isCancelled: true } } },
       { merge: true }
