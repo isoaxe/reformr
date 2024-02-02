d32ca3af348d94eeacc14e09c450b7b4b8807f4c
Lucas O'Connell <lucasoconnell4@gmail.com>
2024-02-02 14:23:34 +0700

    Write the extractFirebaseToken function
    Extracts the Firebase bearer token from the request authorization header. Conducts various checks and returns an error if any fail.

--- src/util/server.js
+++ src/util/server.js
@@ -2,6 +2,7 @@
  * Helper functions available on the server.
  */
 import admin from 'firebase-admin';
+import { headers } from 'next/headers';
 import { initialiseAdmin } from '@/util/admin';
 import { getDocId } from './helpers';
 
@@ -20,4 +21,21 @@ export async function getPatientData(customerId) {
   const { email } = patientData.screening;
   const docId = await getDocId(email);
   return { docId, patientData };
+}
+
+/* Extract and return the Firebase bearer token from HTTPS request headers. */
+export function extractFirebaseToken() {
+  const authorization = headers().get('authorization');
+
+  if (!authorization) return { error: 'No authorization field in header.' };
+
+  if (!authorization.startsWith('Bearer'))
+    return { error: 'Unauthorized due to token missing the Bearer prefix' };
+
+  const split = authorization.split('Bearer ');
+  if (split.length !== 2)
+    return { error: 'Unauthorized due to multiple tokens' };
+
+  const fireToken = split[1];
+  return { fireToken };
 }
