d7518f5ff3c5a49ab3c7fbea29b927ef7e5bbee2
Lucas O'Connell <lucasoconnell4@gmail.com>
2024-02-06 09:00:11 +0700

    Style the program-outline page
    Tweak all styles for the page and it's child step components as they were just raw content previously. Looks much better now.

--- src/app/signup/screening/ws00-program-outline/page.js
+++ src/app/signup/screening/ws00-program-outline/page.js
@@ -6,12 +6,14 @@ import { programOutlineData } from '@/util/data';
 /* Just an explanation of the steps required in the process. */
 export default function ProgramOutline() {
   return (
-    <main className="flex w-full max-w-3xl flex-col">
-      <h1>How our program works</h1>
+    <main className="mx-auto flex w-full max-w-2xl flex-col items-center">
+      <h1 className="mb-4 text-center text-2xl font-semibold text-sky-600 sm:text-3xl">
+        How our program works
+      </h1>
       {programOutlineData.map((step, index) => (
         <Step key={index} {...step} />
       ))}
-      <Button text="Lets Go" link="./ws01-accept-terms" />
+      <Button text="Ok" link="./ws01-accept-terms" />
       <KeyPrompt />
     </main>
   );
--- src/app/signup/screening/ws00-program-outline/step.js
+++ src/app/signup/screening/ws00-program-outline/step.js
@@ -2,17 +2,23 @@ import Image from 'next/image';
 
 export default function Step({ title, image, description }) {
   return (
-    <section>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <div className="relative aspect-square sm:w-1/2">
-        <Image
-          src={image}
-          alt={title}
-          fill
-          className="z-0 object-cover"
-          sizes="24rem"
-        />
+    <section className="mb-3 rounded border border-sky-400 p-2 sm:p-4">
+      <h1 className="mb-2 text-lg font-medium text-slate-700 sm:text-xl">
+        {title}
+      </h1>
+      <div className="flex flex-row items-center">
+        <div className="relative aspect-square w-28">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            className="z-0 rounded object-cover"
+            sizes="24rem"
+          />
+        </div>
+        <p className="ml-2 w-full text-base font-thin sm:ml-4 sm:text-lg">
+          {description}
+        </p>
       </div>
     </section>
   );
