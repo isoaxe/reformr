f356154c955d22f073608a17a6d7cc5f8d1a7f2d
Lucas O'Connell <lucasoconnell4@gmail.com>
2024-03-07 17:19:31 +0700

    Remove 'main' from URL route segments
    Making 'main' a route group will mean that we can apply the layout whilst removing the visible route segment from the URL.

--- src/app/page.js
+++ src/app/page.js
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation';
 /* This replaces <Home/> as the entry point to the app. */
 /* Needed to move <Home/> so that new layout got applied to it. */
 export default function Entry() {
-  redirect('/main/home ');
+  redirect('/home');
 }
