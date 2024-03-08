57010172dc7da120a3b197e2075aac85e63cac0f
Lucas O'Connell <lucasoconnell4@gmail.com>
2024-03-08 22:35:41 +0700

    Add navbar and footer to homepage
    Moving the homepage from /home to root meant that the main layout was no longer applied.
    Just manually add the components from which the main layout is comprised to the homepage to regain lost UI.

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,19 @@ import Process from './(main)/home/process';
 import SalesPitch from './(main)/home/sales-pitch';
 import FAQ from './(main)/home/faq';
 import Closer from './(main)/home/closer';
+import FullNavbar from '@/components/layout/full-navbar';
+import Footer from '@/components/layout/footer';
 
 export default function Home() {
   return (
     <main>
+      <FullNavbar />
       <Hero />
       <Process />
       <SalesPitch />
       <FAQ />
       <Closer />
+      <Footer />
     </main>
   );
 }
