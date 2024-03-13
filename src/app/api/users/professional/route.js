b1506d709be807d8d9f33295c99b28b5836223b1
Lucas O'Connell <lucasoconnell4@gmail.com>
2024-03-13 16:07:16 +0700

    Use Admin SDK to create user from admin dashboard
    Better to use the Admin SDK (firebase-admin/auth) to create a new user from the admin dashboard. This replaces the last implementation of the Client SDK for new user creation.
    The client-side creation of accounts can now be disabled from the Firebase dashboard and this will prevent spam account creation.

--- src/app/api/users/professional/route.js
+++ src/app/api/users/professional/route.js
@@ -1,8 +1,6 @@
 import { getAuth } from 'firebase-admin/auth';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { NextResponse } from 'next/server';
 import { initialiseAdmin } from '@/util/admin';
-import { auth } from '@/util/firebase';
 
 /* Creates a new company user with role of doctor, pharmacist or admin. */
 export async function POST(request) {
@@ -18,10 +16,8 @@ export async function POST(request) {
       return NextResponse.json({ error: 'Invalid role.' });
 
     /* Create new company user. */
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const newUser = res.user;
-    await updateProfile(auth.currentUser, { displayName });
-    await getAuth().setCustomUserClaims(newUser.uid, { role });
+    const user = await getAuth().createUser({ email, password, displayName });
+    await getAuth().setCustomUserClaims(user.uid, { role });
     return NextResponse.json({ error: false });
   } catch (error) {
     console.error('Error creating new user: ', error);
